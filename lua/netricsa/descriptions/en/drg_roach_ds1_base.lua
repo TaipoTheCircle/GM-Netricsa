6fd05f0e85bc5ad51107abf2086ceca6b187790c
solaireismybro <jimmyjenkinsjunior6@gmail.com>
2025-10-14 00:53:12 -0400

    Create drg_roach_ds1_base.lua (Andre of Astora)
    Basically just added a page for Roach's Dark Souls 1 Base addon with his Andre of Astora npc.

---
+++ lua/netricsa/descriptions/en/drg_roach_ds1_base.lua
@@ -0,0 +1,25 @@
+ANDRE OF ASTORA
+
+
+CLASS:      Human (Blacksmith)
+METABOLISM: Human, Male
+PERCEPTION: High (Can sense when someone needs an ass-whooping)
+SIZE:       ~8 ft
+ENDURANCE:  High (Can tank most medieval weaponry)
+HOSTILITY:  Neutral 
+WEAPONS:    Fisticuffs, Kicks
+REWARD:     1000 FC
+THREAT:     High
+
+DESCRIPTION:
+
+Hailing from Lordran, the land of Gwyn, Andre worked passionately at an old, run-down church in the Undead Parish. From there, he served as a blacksmith, providing smithing and repairs to any travelers who needed it. 
+
+Andre boasts a stocky but muscular build, with so much definition to the point that he doesn't even bother with wearing a shirt. While most blacksmiths would typically fight with their hammer, Andre instead uses his hands and god-given strength to pulverize any poor soul who dares to challenge him. Basically, use your common fucking sense and don't even think about opposing this behemoth of a man.
+
+
+TIPS:
+
+- ALWAYS stay on his good side, otherwise the consequences may be fatal.
+- Never lower your guard when fighting Andre; His punches are not to be reckoned with.
+- If he starts to recklessly charge, GET OUT OF THE WAY! He has a nasty dropkick...
