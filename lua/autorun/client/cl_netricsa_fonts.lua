46996072a69f6b796ce72585725dfbe776891a61
MagicFoxG <themagicfoxgames@gmail.com>
2025-10-21 20:48:20 +0300

    REFACTOR!!!
    (cherry picked from commit 209b70dc0304a3ac754c87cb1b103fac5d5a0582)

---
+++ lua/autorun/client/cl_netricsa_fonts.lua
@@ -0,0 +1,22 @@
+if CLIENT then
+    surface.CreateFont("NetricsaText", {
+        font     = "Arial",
+        size     = 16,
+        weight   = 500,
+        extended = true
+    })
+
+    surface.CreateFont("NetricsaTitle", {
+        font     = "Arial",
+        size     = 22,
+        weight   = 700,
+        extended = true
+    })
+
+    surface.CreateFont("NetricsaBig", {
+        font     = "Arial",
+        size     = 48,
+        weight   = 800,
+        extended = true
+    })
+end
