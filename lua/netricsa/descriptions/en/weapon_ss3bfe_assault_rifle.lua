82f6e09c2f9b11d0eaa03e4d08941c9946eadda3
TaipoTheCircle <tails321supsen@gmail.com>
2025-10-21 18:27:23 +0800

    suffering and ss3 weapons (not all)
    (cherry picked from commit b91caa61e2cb1b568f703767b28fca8577417364)

---
+++ lua/netricsa/descriptions/en/weapon_ss3bfe_assault_rifle.lua
@@ -0,0 +1,15 @@
+M29 INFANTRY ASSAULT RIFLE
+
+
+WEAPON: M29 Infantry Assault Rifle
+AMMO:   5.56 mm tungsten-flechette rounds.
+ROF:    up to 600 RPM.
+
+DESCRIPTION:
+
+The EDF’s standard-issue assault rifle - a rugged, mass-produced hybrid design using a gas-operated, rotating-bolt mechanism. Fitted with an elongated barrel for extended range and a reflex sight for fast target acquisition. Hip-fire produces wide spread; aiming down sights yields high accuracy.
+
+NOTES:
+
+- Low recoil, good accuracy when sighted - ideal for medium to long engagements. 
+- Single shot per press, hold for continuous fire; ADS greatly improves grouping.
