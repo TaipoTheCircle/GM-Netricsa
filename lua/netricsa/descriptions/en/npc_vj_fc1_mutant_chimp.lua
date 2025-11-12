7fc89977511c01709de232f12bf5e8514053c4ab
TaipoTheCircle <tails321supsen@gmail.com>
2025-11-12 23:05:46 +0800

    FC 1 ENEMIES DCRPTS (NOT ALL)

---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_mutant_chimp.lua
@@ -0,0 +1,21 @@
+MUTATED CHIMPANZE FROM THE KRIEGER ARCHIPELAGO
+
+
+CLASS:      Trigene Primate, Chimpanzee
+METABOLISM: Trigene - a mutant primate.
+PERCEPTION: Tactical Hunter
+SIZE:       4.1 ft
+ENDURANCE:  Average
+HOSTILITY:  High
+WEAPONS:    Claws, jumping attacks.
+REWARD:     400 FC
+THREAT:     High
+
+DESCRIPTION:
+
+The most monstrously deformed of all primates, trigenes. Retaining only a vague resemblance to chimpanzees, they have acquired reptilian features: an elongated snout, four yellow eyes, and three-fingered limbs with forked digits. Remnants of fur remain only along the spine. Ideally suited for stealth attacks, they can penetrate ventilation shafts thanks to their small size.
+
+TIPS:
+
+- Critically vulnerable to headshots – despite their speed, precision shots are effective.
+- Keep your distance – at close range, their high-speed attacks are almost unstoppable.
