d4e00a64eb7d9a138156dad277317deaf1575d62
TaipoTheCircle <tails321supsen@gmail.com>
2025-11-15 00:24:12 +0800

    more mutants fc 1 dscrpts
    (cherry picked from commit 976a562fd026a9b6748b01e44b19299d0490876b)

---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_pig.lua
@@ -0,0 +1,24 @@
+WILD BOAR FROM KRIEGER ARCHIPELAGO
+
+
+CLASS:      Wild Boar
+METABOLISM: Herbivore/Omnivore
+PERCEPTION: Royalist
+SIZE:       3.5 ft.
+STAMINA:    Low
+HOSTILITY:  Passive
+WEAPOON:    Tusks
+REWARD:     50 FC
+THREAT:     None
+
+DESCRIPTION:
+
+The wild boar is a classic representative of the Krieger Archipelago fauna, prized for its juicy breast. Its characteristic features include a short, stocky body, a long, narrow head, sharp and erect ears. Despite its menacing appearance, it exhibits exceptional timidity and lack of intelligence. At the slightest threat, it flees, offering only interest as a source of food.
+
+It spends its time searching for food, ignoring its surroundings. When approached by humans, it prefers to retreat. It does not attack.
+
+TIPS:
+
+- An excellent food source - one carcass provides several servings of brisket.
+- Approach closely - the animal poses no threat.
+- Shoot without fear - the boar's only reaction will be a violent escape accompanied by dubstep.
