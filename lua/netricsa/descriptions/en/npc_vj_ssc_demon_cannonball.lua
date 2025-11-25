84476a05d4ebd78a7f8ad45fb1e61abd101a9511
TaipoTheCircle <tails321supsen@gmail.com>
2025-11-25 21:01:17 +1400

    ss extra descriptions

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_demon_cannonball.lua
@@ -0,0 +1,21 @@
+FIENDIAN REPTILOID DEMON, CANNONBALL-THROWER
+
+
+CLASS:      Fiendian Reptiloid Demon, Cannonball-Thrower
+METABOLISM: cold-blooded, reptile-like
+PERCEPTION: modified primitive race
+SIZE:       20 ft
+ENDURANCE:  medium
+HOSTILITY:  high
+WEAPONS:    Cannonball projectile, claws
+REWARD:     6000 FC
+THREAT:     medium
+
+DESCRIPTION:
+
+Another hideous mutation, courtesy of Notorius Mental. A simple lizard-like organism from Fíendia Prime was crossbred and mutated through several generations, and was finally reinforced with a titanium skeleton. It has a unique ability of forming a miniature black hole through which it can summon Cannonballs into existence. Weak magical forces will hold this Uranium-filled projectile coherent for a short period of time. As it hits the ground, it will detonate and finally explode.
+
+TIPS:
+
+- Avoid the Cannonballs at all costs!
+- Cannonballs are slower when your farther away so keep your distance.
