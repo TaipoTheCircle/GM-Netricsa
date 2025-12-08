ca7d02fc33608d7d153fa14cb9bb896892d8592b
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-08 23:18:18 +0300

    merc cover fc1 decriptions update

---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_merc_cover_m4.lua
@@ -0,0 +1,21 @@
+M4 STORMTROOPER FROM KRIEGER ARCHIPELAGO
+
+
+CLASS:      M4 Trooper, Krieger Elite Mercenary
+METABOLISM: Human, Male
+PERCEPTION: Standard vision, tactical training
+SIZE:       6 ft
+ENDURANCE:  High
+HOSTILITY:  High
+WEAPOON:    Desert Eagle
+REWARD:     500 FC
+THREAT:     Medium-High
+
+DESCRIPTION:
+
+The most common and recognizable soldier of Dr. Krieger's private military company. They are tall, physically strong professionals recruited from all over the world. Their motivation is money, not ideology. They wear various uniforms and headgear (caps, helmets, and caps), but always wear body armor. They possess excellent firepower and tactical acumen: alone, they attack head-on, and when paired with other units (such as a scout), they can draw fire while an ally flanks. Although many are dissatisfied with the maintenance schedule (heat, insects, bad weather, isolation), they remain disciplined and a dangerous threat. They are especially dangerous when supported by other troop types.
+
+TIPS:
+
+- Armor the corps building. The most effective method is headshots.
+- Avoid flanking maneuvers.
