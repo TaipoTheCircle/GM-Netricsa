da8099137ff0e91d7a20e38fdc098acab6d8b834
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-10 18:21:58 +0300

    fc1 part ????? Animals

---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_glowbug.lua
@@ -0,0 +1,20 @@
+FIREFLY FROM THE KRIEGER ARCHIPELAGO
+
+
+CLASS:      Firefly, Bioluminescent Insect
+METABOLISM: Insect
+PERCEPTION: Photoreception, basic vision
+SIZE:       0,06 ft
+ENDURANCE:  Very low
+HOSTILITY:  None
+WEAPONS:    None
+REWARD:     1 FC
+THREAT:     None
+
+DESCRIPTION:
+
+Small flying insects that inhabit damp and dark areas of the archipelago (jungles, caves, coastlines at night). They possess bioluminescence, emitting a soft light in one of three colors: green (the most common), yellow, and blue. They pose no threat. Their presence serves to create atmosphere and can sometimes illuminate paths or hiding places in the dark.
+
+TIPS:
+
+- They are not enemies or resources. Destroying them is pointless and doesn't provide much benefit.
---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_grasshopper.lua
@@ -0,0 +1,20 @@
+TROPICAL GRASSHOPPER FROM THE KRIEGER ARCHIPELAGO
+
+
+CLASS:      Tropical Grasshopper
+METABOLISM: Insect, Orthoptera
+PERCEPTION: Compound eyes, vibration sensitivity
+SIZE:       0.03 ft
+ENDURANCE:  Extremely low
+HOSTILITY:  None
+WEAPONS:    None
+REWARD:     1 FC
+THREAT:     None
+
+DESCRIPTION:
+
+A small tropical insect that lives in the grass and shrubs of the archipelago. It behaves similar to a fly: when threatened, it makes a short, noisy jump-flight for several meters, then freezes again. It does not attack or interact with other creatures.
+
+TIPS:
+
+- Attacking is pointless. It is not a resource.
