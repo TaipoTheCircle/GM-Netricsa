f2907a16736fd0dae66be7c52d612c60c29518d1
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-13 18:53:13 +0300

    fc1 sonic description

---
+++ lua/netricsa/descriptions/en/npc_vj_fc1_mutant_big_sonic.lua
@@ -0,0 +1,21 @@
+SUPER SANYA FROM THE KRIEGER ARCHIPELAGO
+
+
+CLASS:      Super Sanya, Supersonic Mutant Bruiser
+METABOLISM: Hyper-accelerated Trigen mutant
+PERCEPTION: Slowed time perception, motion stimuli
+SIZE:       7,2 ft
+ENDURANCE:  Very high, Extremely low
+HOSTILITY:  Hyper-aggressive
+WEAPONS:    Supersonic boots, speed ramming, sonic booms, claws
+REWARD:     2500 FC
+THREAT:     Critical
+
+DESCRIPTION:
+
+A unique, flawed byproduct of Dr. Egger's experiments to overcome hydrophobia in Trigens. Instead of being cured, the mutant acquired pathological super-speed. His mutated legs have transformed into biomechanical "supersonic boots" that generate a force field, allowing him to run across water and vertical surfaces, leaving behind an explosive sonic wave. However, his psyche hasn't survived the changes: he suffers from paradoxical hydrophobia and delusions of grandeur, believing himself to be a "gigachad on steroids." In combat, he moves so fast that he leaves behind only a blue blur and a sonic boom. He's as straightforward as all Fat Boys, but his straightforwardness is now measured in kilometers per second.
+
+TIPS:
+
+- He wages a personal war with Dr. Egger, believing him to be responsible for his condition.
+- Like a regular Fat, he has a glowing implant on his chest, but it can only be hit when he stops moving. - Use a shotgun, heavy machine guns, or area-effect weapons (such as a flamethrower) to cover the area where he might be.
