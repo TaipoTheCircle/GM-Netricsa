6aaaa56f3b98ce4f3198ee3080179e72025610de
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-14 03:50:42 +0300

    zenozoik descriptions part 1

---
+++ lua/netricsa/descriptions/en/npc_desert_worm.lua
@@ -0,0 +1,21 @@
+SANDWORM FROM THE ZENOZOIK
+
+
+CLASS:      Sandworm, Predatory Coprolite
+METABOLISM: Xenobiology, Organosilicon
+PERCEPTION: Vibrational, Chemical
+SIZE:       3,9 ft
+ENDURANCE:  Average
+HOSTILITY:  Extreme (territorial)
+WEAPONALS:  Drilling jaws, rapid lunge from beneath the sand
+REWARD:     100 FC
+THREAT:     High
+
+DESCRIPTION:
+
+Aggressive subterranean predators that inhabit the loose sands of desert dunes. Their presence is revealed by the characteristic shifting, "unstable" texture of the sand on the surface. The worms are almost constantly in motion underground, forming distinctive "sand rivers." They attack with lightning speed, bursting out of the sand like a tentacle, their mouths wide open and studded with bony plates.
+
+TIPS:
+
+- If you see a "hump" moving under the sand, it's a worm hunting.
+- Attack immediately after its lunge—at that moment, it's vulnerable to attacks.
---
+++ lua/netricsa/descriptions/en/npc_hen.lua
@@ -0,0 +1,22 @@
+HEN FROM ZENOZOIK
+
+
+CLASS:      Hen
+METABOLISM: Avian
+PERCEPTION: Basic vision, sensitivity to sudden movements
+SIZE:       1,5 ft
+ENDURANCE:  Extremely low
+HOSTILITY:  Virtually absent (panicked flight)
+WEAPONS:    Beak
+REWARD:     2 FC
+THREAT:     Negligibly low
+
+DESCRIPTION:
+
+A wild bird common in settlements and on the outskirts of Zenozoik. Unlike crows, chickens are distinguished by extremely low intelligence and a complete lack of fighting ability. Their behavior is chaotic flight at any threat. They are famous for being the object of a peculiar "ritual" or act of brute force in the world of Zenozoik - their heads are often torn off. They don't gather in aggressive flocks, but they can create panic by swarming around.
+
+TIPS:
+
+- Attacking them is pointless and can attract unnecessary attention.
+- Completely defenseless. They pose no threat, but they can accidentally provoke aggression from other creatures if they are hunted.
+- There are rumors of chickens that burst into flames.
