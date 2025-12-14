4620ff8ccab851eae4886ef9f7bf8628e04d7361
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-14 22:55:35 +0300

    zenozoik descriptions part 2

---
+++ lua/netricsa/descriptions/en/npc_zeno_bugbird.lua
@@ -0,0 +1,22 @@
+BUGBIRD FROM THE ZENOZOIK
+
+
+CLASS:      Bugbird, Trained Xenobiology
+METABOLISM: Xenobiological
+PERCEPTION: Red eyes, hearing, movement
+SIZE:       1,3 ft
+ENDURANCE:  Average
+HOSTILITY:  Average
+WEAPONS:    Jagged beak, clawed feet, jumping attacks
+REWARD:     40 FC
+THREAT:     Average
+
+DESCRIPTION:
+
+A small hybrid creature popular as a guard or ornamental pet in Zenozoik cities. It has smooth gray-brown skin on its head, an elongated beak-like mouth lined with small teeth, and bright red eyes. Its body is covered in brown fur with light, wavy stripes. It has four limbs with large claws and a pointed tail. Contrary to what you'd expect from a pet, it moves in a sharp, erratic zigzag pattern, and when threatened, it makes unexpected leaps, making it a difficult target. Its diet presumably consists of insects (bugs), although this hasn't been confirmed. Upon seeing an enemy, it becomes aggressive, using its speed and leaps to quickly bite and scratch.
+
+TIPS:
+
+- If it jumps, you can deliver a powerful kick to the head.
+- Despite its speed, its leaps are predictable.
+- Use area-of-effect weapons (shotguns, short-range explosive grenades) or rapid-fire automatic weapons to cover its potential range.
