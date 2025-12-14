4d74299f34c665e1520664a25de9cd2a3cb630c2
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-14 17:36:55 +0300

    smirnof

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_bighead_smirnof.lua
@@ -0,0 +1,30 @@
+BIG-HEAD, S-199 SMIRNOF
+
+
+CLASS:      Big-Head, S-199 Smirnof
+METABOLISM: Humanoid, Male
+PERCEPTION: Enthusiastic Fanatic
+SIZE:       5,9 ft
+ENDURANCE:  Low
+HOSTILITY:  Neutral
+WEAPONS:    None (except on I Am Ugh-Zan difficulty)
+REWARD:     7777 FC
+THREAT:     Annoyance
+
+DESCRIPTION:
+
+Big-Heads are thin humanoids with exaggerated skulls containing the brains of the developers. They wear the uniform of Serious Sam, but have the unique facial features of the development team. They display an obsessive enthusiasm, chasing you with cries of "Hello!" and "Hey, Wait!" They're completely harmless, but on the "I Am Ugh-Zan" difficulty level, they gain fire and ice modifiers. They spawn spontaneously in secret areas, blocking passages with their sheer numbers.
+
+S-199 Smirnof isn't from Croteam. He grew up in the gmod workshop. He started out making simple nextbots, then learned to import exact replicas of Call of Duty weapons into GMod. His brain works like this: he sees something in the game and understands how to assemble it.
+
+He met Jack the Indifferent, and it all started. Together, they're putting together the entire Far Cry 1 game in GMod. His famous work is "(VJ BASE) Quake 4 SNPCs." He's the one who writes clear descriptions for his weapons for GM-Netricsa by Typo. He helped with Serious Sam: Classic SNPCs (EXTRA), fixing some NPCs. Teaches Typo how to fix textures and ragdolls in Blender.
+
+He and Jack built the scrapped Sniper Assassin from The Suffering. They sometimes argue over trivial things, but they always make up—friendship is more important.
+
+He doesn't like making new acquaintances. His description clearly states: "Don't just message me as a friend. Tell me why first. If you just want to be friends, I'll turn you down." His circle consists of Jack, Typo, and his old friend Barsik, with whom he plays games. He's polite to others, but keeps his distance. He's not mean; he simply values ​​his time and energy for those he already cares about.
+
+TIPS:
+
+- Use an army knife or pistol to clear a path.
+- On the "I Am Ugh-Zan" difficulty, destroy them remotely due to their modifiers.
+- A sledgehammer with a horizontal strike is effective in a group.
