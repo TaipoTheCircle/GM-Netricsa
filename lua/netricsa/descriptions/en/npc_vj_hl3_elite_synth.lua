df7e695f963d4c171e6d54708f281e79de9c3be2
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-30 20:39:02 +0300

    half-life 3 part final

---
+++ lua/netricsa/descriptions/en/npc_vj_hl3_elite_synth.lua
@@ -0,0 +1,23 @@
+COMBINE ELITE SUPER SOLDIER
+
+
+CLASS:      Synth Elite Heavy Assault Unit
+METABOLISM: Synth
+PERCEPTION: Tactical sensors + integrated tracking systems
+HEIGHT:     13 ft
+ENDURANCE:  Very high
+HOSTILITY:  Extreme
+WEAPONS:    Primarily heavy weapons / integrated missiles, sometimes cannons
+REWARD:     1500 FC
+THREAT:     Critical
+
+DESCRIPTION:
+
+The Combine Elite Super Soldier is a combat synth unit, originally designed as an elite infantry trooper.
+
+Essentially a bipedal creature with massive armor and an emphasis on close-quarters attack.
+
+TIPS:
+
+- It's best not to argue with such a colossus.
+- Use RPG V2, it will take 2-3 perfect hits to completely eliminate him.
---
+++ lua/netricsa/descriptions/en/npc_vj_hl3_freeman.lua
@@ -0,0 +1,27 @@
+GORDON FREEMAN
+
+
+CLASS:      Physicist-Resistance Warrior
+METABOLISM: Human, Male
+PERCEPTION: Standard Human + Scientific Intuition
+SIZE:       5,9 ft
+ENDURANCE:  Very High
+HOSTILITY:  High
+WEAPONS:    From a crowbar to heavy weapons
+REWARD:     10000 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+Gordon Freeman is a theoretical physicist in a H.E.V. Suit who found himself embroiled in humanity's most dangerous events. He was an employee of Black Mesa and participated in the experiment that led to the Cascade Resonance and the invasion of alien life on Earth.
+
+Freeman is a man of few words, but he is determined. During the Black Mesa disaster, he was forced to survive, fight alien creatures, and defend himself against H.E.C.U. forces.
+
+Ultimately, he was able to defeat the Nihilanth, who was keeping the rift open on the other side of the conflict.
+
+Years after the events of Black Mesa, he became a key figure in the Resistance against the Combine, a symbol of hope and resistance to the occupation of Earth.
+
+TIPS:
+
+- He is a master of a wide variety of weapons, from a crowbar to heavy machine guns.
+- Gordon survives through tactical thinking, adaptation, and mastery of weapons.
