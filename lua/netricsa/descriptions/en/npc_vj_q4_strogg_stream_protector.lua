f3233924368e5629290eeb982749f06b7f61e9dc
TaipoTheCircle <tails321supsen@gmail.com>
2025-12-31 03:34:26 +0300

    С НОВЫМ ГОДОМ!

---
+++ lua/netricsa/descriptions/en/npc_vj_q4_strogg_stream_protector.lua
@@ -0,0 +1,24 @@
+STROGG STREAM PROTECTOR FROM STROGGOS
+
+
+CLASS:      Elite Strogg Combat Unit
+METABOLISM: Organic / Biomechanical Warrior
+PERCEPTION: Combat Vision System + Thermal and Vibrational Sensors
+SIZE:       8,2 ft
+ENDURANCE:  Very High
+HOSTILITY:  Extreme
+WEAPONS:    Plasma Spike Cannon, Homing Missiles
+REWARD:     2000 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+The Stream Defector is an elite Strogg guardian created to protect key energy and strategic facilities on the planet Stroggos. These creatures act as defenders of the Flow and the Nexus, tasked with preventing the destruction of complex infrastructure complexes by invading human forces.
+
+In combat, the Stream Defector combines a successful combination of long-range and close-range fire. It is armed with a powerful plasma cannon, capable of delivering sporadic fire, and homing missiles that pursue the enemy at the slightest movement.
+
+TIPS:
+
+- Use heavy weapons to effectively destroy missile attacks.
+- Maneuvering and cover are critical: a direct, positional attack is rarely successful.
+- Their missiles can be destroyed in flight if you act quickly and accurately.
