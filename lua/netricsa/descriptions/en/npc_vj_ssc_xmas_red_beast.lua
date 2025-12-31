9f54f398d632800e727ca5dce5c3423e583ec06f
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-01 00:33:35 +0300

    Xmas 2025-2026  Reptiloid Description

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_xmas_red_beast.lua
@@ -0,0 +1,29 @@
+ALUDRAN REPTILOID, CHRISTMAS (RED)
+
+
+CLASS:      Aludran Reptiloid, holiday subspecies
+METABOLISM: Cold-blooded, reptile-like
+DEFINITION: Primitive race, member of the New Year cult
+SIZE:       15 ft
+ENDURANCE:  Average
+HOSTILITY:  High
+WEAPONS:    Magic homing missiles (red fireballs)
+REWARD:     5500 FC
+DANGER:     Average
+
+DESCRIPTION:
+
+A large fourhanded reptile living in the Aludran system in the constellation of Canis Major. This primitive race has agreed to fight for Mental in exchange for the spell-casting powers it gave them.
+
+They cast self-propelled fireballs with primitive homing abilities. 
+
+The Christmas Red Aludran Reptiloid is a rare and disturbingly grotesque mutation of the plains species. Unlike its brethren, this subspecies has been doubly influenced: by the magic of the Mental and by an entity known as the Giant Snowman, who promised an Eternal New Year. The price of the deal turned out to be symbolic and humiliating - the obligatory wearing of a Santa Claus New Year's hat.
+
+They are easily recognizable in appearance: red magic balls instead of the usual green ones, red blood, and a bright red bow on their massive chests, secured with crude magic. Despite their comical appearance, they remain extremely dangerous in battle.
+
+The reptilians themselves sincerely believe that they are fighting for an eternal holiday. Ironically, the New Year truly is endless for them – but only until they die fighting for someone else's war.
+
+TIPS:
+
+- Keep them under constant fire and try to shoot down the missiles in the air.
+- Red balls are more visible, shoot preemptively.
