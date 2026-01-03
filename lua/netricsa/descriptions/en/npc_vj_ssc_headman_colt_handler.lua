036732663d3d4099b0c1655b4fdfa6e430f4cd49
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-03 19:59:46 +0300

    beheaded colt handler

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_headman_colt_handler.lua
@@ -0,0 +1,24 @@
+BEHEADED COLT HANDLER
+
+
+CLASS:      Beheaded Colt Handler
+METABOLISM: cyber-zombie
+PERCEPTION: insentient, LCU controlled
+SIZE:       6 ft
+ENDURANCE:  medium
+HOSTILITY:  high
+WEAPONS:    colt (TMAR)
+REWARD:     210 FC
+THREAT:     medium
+
+DESCRIPTION:
+
+An executed Sirian soldier, raised from the dead by Mental and remote controlled by Life Control Unit (LCU).
+
+The Colt handler carries his own head in one hand to receive audio / visual information, using the Colt (TMAR) as their primary weapon.
+
+TIPS:
+
+- No known problems.
+- Effective on long range.
+- Do not using projectiles, instant damage.
