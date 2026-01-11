b957ffc42309149d46c95fab299487cbda9f8878
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-11 03:25:56 +0300

    werebull meat and hatching

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_werebull_meat.lua
@@ -0,0 +1,25 @@
+SIRIAN WEREBULL, MEAT
+
+
+CLASS:      Sirian Werebull, Meat
+METABOLISM: Mammal, mutant
+DEFINITION: Draft, Meat animal
+SIZE:       9 ft
+ENDURANCE:  Medium
+HOSTILITY:  High
+WEAPONATAL: Horns
+REWARD:     3500 FC
+THREAT:     High
+
+DESCRIPTION:
+
+A genetic hybrid of a Sirian and a bovine species. The Werebull's behavior is similar to that of a normal bull: it charges at full speed, aiming to gore its opponent.
+
+The Werebull Meat Bull is the result of Mental's devising: he skinned several werebull mounts for his and his colleagues' clothing. The bull is completely skinned, revealing muscles and tendons. Its eyes are missing, yet it is still incredibly sensitive to threats around it. It is as vicious as a normal bull, because every cell in its body senses pain and anger when exposed to air.
+
+In combat, it attacks with a running charge, throwing its victim onto its horns and inflicting lacerations. Despite its terrifying appearance, this bull is considered a "delicacy" on the planet Sirius—though no one who has encountered it on the battlefield has been in a hurry to try it.
+
+TIPS:
+
+- Move out of the attack's line of fire.
+- Looks like a stew from a distance, but it is actually Mental's deadly weapon.
