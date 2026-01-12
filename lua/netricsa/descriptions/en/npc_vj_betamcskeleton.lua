5dfcfb1b58fde5b429c0b29a5a94b7512df9a1e0
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-13 01:11:02 +0300

    minecraft ++ descriptions

---
+++ lua/netricsa/descriptions/en/npc_vj_betamcskeleton.lua
@@ -0,0 +1,24 @@
+BETA SKELETON ARCHER FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:      Beta Skeleton Archer
+PERCEPTION: Undead, ranged unit
+METABOLISM: Magically sustained undead
+SIZE:       6,2 ft
+ENDURANCE:  Average
+HOSTILITY:  High
+WEAPONS:    Bow and arrows
+REWARD:     200 FC
+THREAT:     Average
+
+Description:
+
+The Beta Skeleton Archer is a classic example of how a minimum of flesh doesn't compromise maximum irritation. It's a living collection of bones, held together by magic, malice, and an excellent sense of distance. It dislikes close combat, preferring to methodically turn its target into a cushion for its arrows.
+
+In daylight, the skeleton burns, which, however, doesn't stop it from trying to finish you off with a final arrow as it burns out beautifully.
+
+Tips:
+
+- A shield is your best defense against a skeleton archer.
+- Use cover, otherwise the shooting will be one-sided.
+- If a skeleton suddenly finds itself riding a spider, consider it a promotion.
