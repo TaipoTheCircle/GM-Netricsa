2956adbe61a116047820fc9488b36542c1f0d64c
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-12 17:13:57 +0300

    new

---
+++ lua/netricsa/descriptions/en/npc_mine_dzhigurper.lua
@@ -0,0 +1,24 @@
+CREEPER-DZIGURDA FROM THE MINECRAFT OVERWORLD 
+
+
+CLASS:      Dzigurda Creeper
+METABOLISM: Plant-Chemical
+PERCEPTION: Detectable by Vision
+SIZE:       5,5 ft
+ENDURANCE:  Low
+HOSTILITY:  Extreme
+WEAPONS:    Self-Detonation with Soul Scream
+REWARD:     500 FC
+THREAT:     Medium-High
+
+DESCRIPTION:
+
+This creeper isn't just an explosive mob from the Minecraft Overworld. No, it's possessed by Nikita Dzigurda himself, making it an extremely... emotional creature. It creeps up on you, emitting terrifying screams.
+
+When Dzigurda Creeper explodes, its body disintegrates into a multitude of miniature Dzigurda faces, which continue to scream.
+
+TIPS:
+
+- Don't stand too close—the explosion not only deals damage but can also stun.
+- Trying to move too slowly will result in... Dzhigurda screaming.
+- If you have noise-canceling headphones, put them on immediately.
---
+++ lua/netricsa/descriptions/en/npc_vj_chargedcreeper.lua
@@ -0,0 +1,24 @@
+CHARGED CREEPER FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:      Charged Creeper
+METABOLISM: Electromagnetic-Plant-Chemical
+PERCEPTION: A beefed-up version of the standard Creeper
+SIZE:       5,5 ft
+ENDURANCE:  Medium
+HOSTILITY:  Extreme
+WEAPONATAL: Larger Electric Blast
+REWARD:     700
+THREAT:     Medium-High
+
+DESCRIPTION:
+
+The Charged Creeper is the same green figure as the standard Creeper, only its body is now filled with unbridled electrical energy.
+
+It explodes with greater force than normal, damaging all blocks and mobs within a radius.
+
+Charged Creepers are created by lightning strikes.
+
+TIPS:
+
+- The damage radius and defense are twice that of the standard Creeper.
