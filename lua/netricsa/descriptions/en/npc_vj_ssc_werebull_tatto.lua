ded000299c38f6524236de7f3af776444c365d9b
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-13 04:03:16 +0300

    tatto werebull

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_werebull_tatto.lua
@@ -0,0 +1,23 @@
+SIRIAN TATTO WEREBULL
+
+
+CLASS:      Sirian Tatto Werebull
+PERCEPTION: Domesticated Draft Beast
+METABOLISM: Mammal
+SIZE:       9 ft
+STAMINA:    Low
+HOSTILITY:  Average
+WEAPONS:    Horns, intimidating gaze
+REWARD:     2500 FC
+THREAT:     Average
+
+DESCRIPTION:
+
+A genetic hybrid of a Sirian and a bovine species. The Werebull's behavior is similar to that of a normal bull: it charges at full speed, aiming to gore its opponent.
+
+The Sirian Werebull Tatto is a standard Sirian werebull, but with a bit of artistic flair: the Primitive Gnaar tribes painted a yellow snake with huge red scratches on its flanks, so that opponents understand that it's both serious and brutal.
+
+TIPS:
+
+- Move out of the attack's line of sight.
+- If you see it sniffing the bushes, it's likely looking for inspiration for its next tatto.
