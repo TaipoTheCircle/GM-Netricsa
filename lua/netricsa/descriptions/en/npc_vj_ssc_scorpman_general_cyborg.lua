ff29ffc18de365abb677ad6f8ff3b8a168d47d61
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-13 23:04:44 +0300

    scorpman fix

--- lua/netricsa/descriptions/en/npc_vj_ssc_scorpman_general_cyborg.lua
+++ lua/netricsa/descriptions/en/npc_vj_ssc_scorpman_general_cyborg.lua
@@ -1,24 +1,30 @@
-ARACHNOID, ADULT FROM THE SCORPIUS
+CYBORG ARACHNOID, ADULT FROM THE SCORPIUS
 
 
-CLASS:      Arachnoid, Adult
-METABOLISM: Ectothermous, arthropod
+CLASS:      Cyborg Arachnoid, Adult
+METABOLISM: Ectothermous, cybernetically enhanced arthropod
 PERCEPTION: Primitive sentient race
 SIZE:       20-25 ft
 ENDURANCE:  High
 HOSTILITY:  Extreme
 WEAPONS:    Chain-gun, tail sting
-REWARD:     5000 FC
+REWARD:     8500 FC
 THREAT:     High
 
 DESCRIPTION:
 
 A holy warrior from a primitive race evolved from arthropods. Extremely hostile due to their religious convictions.
+
 Carries a chaingun with ammo-replenisher in one hand. Exoskeleton covers entire Arachnoid's body and provides good protection against bullets, explosives and fire.
+
 The Adults are full grown warriors, having a strong, "ripe" red exoskeleton and a much larger size.
+
+An adult cyborg arachnoid - a Scorpius Holy Warrior, voluntarily (or not) enhanced by Mental's technology.
+
+Part of the body has been replaced with mechanical segments, such as reinforced limbs, servos, recoil stabilizers, and an exoskeleton. This armor can withstand more damage than a standard arachnoid exoskeleton.
 
 TIPS:
 
 - Dangerous sting - do not approach too closely.
 - Will shoot through their own ranks in fury.
-
+- Close combat is a bad idea, a very bad idea.
