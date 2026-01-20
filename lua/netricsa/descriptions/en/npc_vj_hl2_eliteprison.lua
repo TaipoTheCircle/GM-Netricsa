2ed250e5aa24174a8f529cc36a0ffb435fb45bbb
TaipoTheCircle <tails321supsen@gmail.com>
2026-01-20 21:59:26 +0300

    hl 2 elites descriptions

---
+++ lua/netricsa/descriptions/en/npc_vj_hl2_eliteprison.lua
@@ -0,0 +1,24 @@
+COMBINE OVERWATCH ELITE PRISON SOLDIER
+
+
+CLASS:      Combine Overwatch Elite Prison Soldier
+METABOLISM: Augmented Human, Male
+PERCEPTION: Combine Prison Guard
+SIZE:       5,9 ft
+ENDURANCE:  Very High
+HOSTILITY:  Extreme
+WEAPONS:    AR2, Grenades
+REWARD:     900 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+The Overwatch Combine Elite Prison Soldier is a specialized operative designed to guard prisons and manage dangerous inmates. His armor is standard Nova Prospekt, providing enhanced protection, and his helmet is taken from the Combine Elite Soldier, but with a yellow visor, allowing him to observe prisoners and quickly respond to threats.
+
+This type of fighter is equipped with a combination of long-range and close-range weapons: the AR2 pulse rifle allows for suppression of resistance at various distances, while grenades are effective at knocking out cover. Augments provide endurance, reaction speed, and the ability to fight in confined prison spaces.
+
+TIPS:
+
+- The yellow visor reveals his position in the dark, but helps him aim accurately.
+- Keep his distance to avoid close combat.
+- Grenades are dangerous in confined prison corridors; use cover.
---
+++ lua/netricsa/descriptions/en/npc_vj_hl2br_elite.lua
@@ -0,0 +1,24 @@
+COMBINE OVERWATCH ELITE BETA SOLDIER
+
+
+CLASS:      Combine Overwatch Elite Beta Soldier
+METABOLISM: Augmented Human, Male
+PERCEPTION: Combine Ground Operations
+SIZE:       5,9 ft
+ENDURANCE:  High
+HOSTILITY:  Extreme
+WEAPOON:    AR2 Pulse Rifle, Energy Grenades
+REWARD:     900 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+The Combine Elite Beta Soldier is the standard white Overwatch Combine Elite soldier, designed to suppress resistance and maintain order in any environment. Distinguished by their armor, which is entirely white with black accents for camouflage. Unlike other Elite soldiers, he has two red eyes built into his helmet and three vents in his respirator that filter air.
+
+This soldier type is designed for medium and long range combat, combining accurate pulse rifle fire with the use of energy grenades to suppress cover. Augments enhance reaction time and endurance, allowing the soldier to withstand intense fire and fight in difficult conditions.
+
+TIPS:
+
+- Two red eyes make him highly visible in the dark, but also allow him to better react to player movement.
+- Energy grenades can knock you out of cover and incinerate you – don't stand still.
+- Stay behind cover and use long-range weapons to reduce the risk of close combat.
