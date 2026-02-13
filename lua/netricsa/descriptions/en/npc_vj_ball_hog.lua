b0bb2826d5bb32abf6af41646c13e37754c6fd7b
TaipoTheCircle <tails321supsen@gmail.com>
2026-02-13 03:20:54 +0300

    ball pig

---
+++ lua/netricsa/descriptions/en/npc_vj_ball_hog.lua
@@ -0,0 +1,26 @@
+BALL PIG FROM THE MOBIUS 
+
+
+CLASS:      Ball Pig
+METABOLISM: Mechanical Badnik
+PERCEPTION: Combat robot bomber
+SIZE:       ~4 ft
+ENDURANCE:  Low
+HOSTILITY : Medium
+WEAPONS:    Metal ball bombs
+REWARD:     150 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+The Ball Pig is a Badnik robot created by the evil Dr. Eggman as part of his machine army. Its design resembles a pig-like mechanical organism with a rounded body, standing on two mechanical legs and armed with a device built directly into its belly. It's through this circular armor that it fires metallic ball bombs that bounce and damage targets.
+
+Its attack method is simple but effective in narrow corridors and on inclines: the Ball Pig often climbs higher on slopes or at the edge of platforms and drops its bombs down, where they bounce along the ground and twist, creating a deadly trap for anyone trying to pass by.
+
+Despite its appearance, inspired by a real pig, it's a cold war machine. It was among the early Badnik models used during the first Eggman invasions, and it still appears in various worlds today.
+
+TIPS:
+
+- It's best to attack it from a distance or from above.
+- The bombs bounce—anticipate their trajectory and dodge.
+- When destroyed, the Ball Pig often releases an animal hostage, like other Badniks.
