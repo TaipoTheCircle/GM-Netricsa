bd9cab507e8cf3a3c33f1d9257ce8e3a7db5c1d5
TaipoTheCircle <tails321supsen@gmail.com>
2026-02-13 23:08:22 +0300

    ru Strogg Fix

---
+++ lua/netricsa/descriptions/en/npc_vj_q4_strogg_heavyhovertank.lua
@@ -0,0 +1,25 @@
+STROGG HEAVY HOVER TANK FROM STROGGOS
+
+
+CLASS:      Strogg Heavy Hover Tank
+METABOLISM: Cybernetic / Tactical Suppressor
+PERCEPTION: Mobile Hardened Node
+SIZE:       6,6 ft
+ENDURANCE:  Extremely High
+HOSTILITY:  High
+WEAPONS:    Guided Missiles (Right Arm), Rapid-Fire Blaster (Left Wrist), Power Strike (Melee)
+REWARD:     2500 FC
+THREAT:     Extreme
+
+DESCRIPTION:
+
+When the Strogg needed a device capable of taking out both infantry and vehicles with equal effectiveness, they took a heavy infantryman and tore off his legs. They replaced them with an anti-gravity platform. The result is a Heavy Hover Tank—a flying fortress that doesn't run, but hovers.
+
+The Guided Missile is the Heavy Hover Tank's primary attack. It's highly destructive, dealing heavy damage and knocking enemies back several dozen meters with explosive damage and sometimes fall damage.
+
+In close combat, if some madman gets too close, the Tank will simply crush them with its hand.
+
+TIPS:
+
+- Only powerful weapons, such as the Rocket Launcher, Nail Gun, and Dark Matter Cannon, can cause serious damage to it.
+- When fighting other enemies, the Heavy Hover Tank should be destroyed first, as it can inflict significant damage.
