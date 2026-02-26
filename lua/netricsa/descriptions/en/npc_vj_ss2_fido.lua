561a7523f4257849ac2553c1a7c3972838b62966
TaipoTheCircle <tails321supsen@gmail.com>
2026-02-27 01:30:58 +0300

    fido and albino

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_fido.lua
@@ -0,0 +1,26 @@
+FIDO THE SPIKY BULLDOG
+
+
+CLASS:      Fido the Spiky Bulldog
+METABOLISM: Mammal
+PERCEPTION: Guard dog
+SIZE:       3 ft
+ENDURANCE:  low
+HOSTILITY:  high
+WEAPONS:    bites
+REWARD:     75 FC
+THREAT:     low
+
+DESCRIPTION:
+
+Fido dogs are among the most popular breed within Mental's Empire, and are extensively used as both guard and attack dogs. Or at least that's what most humans think.
+
+Fido dogs are actually nothing more than domesticated pets that will attack those who threaten their owners. Mental's forces are permitted to bring their dogs to them in combat, kind of like how humans take their dogs out for a walk, only with more guns and blood.
+
+Fortunately, their bark is worse than their bite; just stay away and gun 'em down.
+
+TIPS:
+
+- Jodge their charges by side-stepping or jumping over them.
+- Try to line them up; they will trample over each other as they try to get to you.
+- Use rapid-firing or area-damage weapons to take them out quickly.
