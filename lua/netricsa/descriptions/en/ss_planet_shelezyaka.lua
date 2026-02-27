79ecfaef5c28b0f495066149fbfb74f3fb583a18
TaipoTheCircle <tails321supsen@gmail.com>
2026-02-28 01:50:55 +0300

    SHELEZYAKA

---
+++ lua/netricsa/descriptions/en/ss_planet_shelezyaka.lua
@@ -0,0 +1,30 @@
+SHELEZYAKA
+
+
+PLANET:       Shelezyaka
+TYPE:         Terrestrial planet, Factory planet
+CLIMATE:      Airless space, temperature fluctuations
+MOONS:        Unknown
+TERRAIN:      A flat sulfuric plain covered in dust
+LOCATION:     A distant part of the Milky Way Galaxy, off the beaten path, near the planet Sheshineru
+NATIVE RACES: Robots (descendants of the crew of a crashed robotic ship), Robobeasts
+ALLIANCE:     Independent robotic civilization
+LOCATIONS:    Desert spaceport, Robot settlements, Robobeast habitats
+
+DESCRIPTION:
+
+Shelezyaka is a planet that is a clear example of the complete transformation (and depletion) of the world by a technological civilization. This was once an ordinary world with an atmosphere, water, seas, mountains, vegetation, and animals. Many years ago, an automated ship carrying obsolete universal service robots crashed here.
+
+The surviving robots began to actively reproduce, using the local mineral resources. During the process of industrialization, they completely depleted the planet's resources: oxygen was burned in furnaces, water was eliminated (to prevent rust and used to cool engines), mountains were razed, and seas were drained. By now, Shelezyaka has become a gray, flat sphere, covered in dust, without an atmosphere or any signs of life.
+
+A famous entry in the galactic directory reads: "No mineral resources. No water. No vegetation. Inhabited by robots.".
+
+The natives are the descendants of those very robots. They are short, heavy, strong, and durable ("built to last"). They are straightforward, but capricious and easily offended. Each one has an indicator light on their forehead. They are skilled craftsmen, but their civilization remains at a primitive technological level, without computers or agriculture. Their favorite delicacy is sunflower oil.
+
+When the planet's wildlife died out due to unsuitable conditions, robots created mechanical animals to fill the void. Later, when parts ran short, the robotic animals were dismantled, and they retreated into a semi-wild state. They are divided into "mouse" and "cat" types; all move on wheels and can squeak, while the robo-cats can lick with an iron tongue. They are tamable.
+
+KEY LOCATIONS:
+
+- Deserted Cosmodrome: The only landing site, always dusty and deserted. Over the years, only one visitor has visited here - the space pirate Rat.
+- Robot Settlements: Areas inhabited by the planet's indigenous inhabitants. Robots love to give advice and, when not working, prefer to sit or lie down.
+- Robo-beast habitats: Territories inhabited by feral mechanical animals. Here you can encounter flocks of wheeled robo-mice or stealthy robo-cats.
