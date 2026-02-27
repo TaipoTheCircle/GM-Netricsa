72c7cae0f9026be24e0df2f546df984df46178fb
TaipoTheCircle <tails321supsen@gmail.com>
2026-02-28 01:10:37 +0300

    turret enemy

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_turret_machinegun_enemy.lua
@@ -0,0 +1,32 @@
+MH-110 MACHINE GUN TURRET
+
+
+CLASS:      MH-110 Machine Gun Turret
+METABOLISM: Mechanical 
+PERCEPTION: Stationary
+SIZE:       ~3,2 ft
+ENDURANCE:  Average
+HOSTILITY:  Varies with the shooter
+WEAPONS:    Twin 110mm machine guns
+REWARD:     300 FC
+THREAT:     Average
+
+DESCRIPTION:
+
+When Mental designed his bases, he based his thinking on a simple principle: "Everything that moves must be either a slave or a corpse". For those who refused to become either, he developed stationary machine gun turrets - twin barrels capable of grinding up flesh faster than the enemy even realized it.
+
+Most agreed on the first option.
+
+It has two machine guns, paired together, and an infinite ammo supply (the crate is the source of the magical ammo creation), as well as a targeting system that allows even those who have never seen a weapon before to fire.
+
+The turret's rate of fire is such that in a matter of seconds it can turn a pack of Gnaars into a bloody mess, or a Cyclops into a sieve.
+
+Turrets are usually installed in Mental's bases, which can be captured and used against their master.
+
+If an enemy is behind the turret, it will rain bullets on anything that moves. If you dislodge the enemy and jump in yourself, you can set off a fireworks display.
+
+TIPS:
+
+- If you see a turret with an enemy, take them out first. A turret without a gunner is just a piece of metal.
+- Against fast creatures, a turret is indispensable. Bullets reach you instantly.
+- If you see someone's orcish legs sticking out from behind a turret, it means there's someone there.
