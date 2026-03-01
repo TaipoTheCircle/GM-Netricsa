abbabe9a224b70ff5137456501344f837e14ca1f
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-01 03:15:48 +0300

    ice fix and and turok mini-raptor

---
+++ lua/netricsa/descriptions/en/npc_turok_miniraptor.lua
@@ -0,0 +1,25 @@
+MINI-RAPTOR FROM THE TERRAFORMED PLANET
+
+
+CLASS:      Mini-Raptor
+METABOLISM: Predator, Theropod
+PERCEPTION: Small pack dinosaur
+SIZE:       ~2,7 ft
+ENDURANCE:  Very low
+HOSTILITY:  High1
+WEAPONS:    Claws, teeth, speed, numbers
+REWARD:     150 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+These are a new generation of compsognathus, only blue and with large eyes. Their bright blue skin, orange feather growths, and disproportionately large eyes make them look like alien parrots who decided meat is tastier than sunflower seeds.
+
+On their own, a Mini-Raptor is harmless. One kick, one shot from any gun, and all that remains of the little dinosaur is feathers. The problem is, they never come alone. Mini-Raptors are pack animals, and a pack isn't considered "five or six individuals".
+
+When a target appears, they rush in as a group, trying to surround, pin down, and bite it to death.
+
+TIPS:
+
+- One shot, one kill.
+- Don't let yourself get surrounded.
