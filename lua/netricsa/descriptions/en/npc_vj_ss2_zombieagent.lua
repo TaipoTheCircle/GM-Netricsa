af30882b5f840052da0790ce913d34031f4fa737
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-02 23:05:52 +0300

    zombie broker

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_zombieagent.lua
@@ -0,0 +1,27 @@
+ZOMBIE STOCKBROKER
+
+
+CLASS:      Zombie Stockbroker
+METABOLISM: Undead
+PERCEPTION: Zombie Soldier
+SIZE:       6 ft
+ENDURANCE:  Low
+HOSTILITY:  Medium
+WEAPONS:    Pump-Action Shotgun
+REWARD:     50 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+This is a special unit of the Mental Ministry of Finance. They roam conquered worlds, seeking to "make deals" with local businessmen. And by "make deals", I mean stealing money or shooting a businessman in the face and then stealing it.
+
+In a cruel twist of humor, businessmen killed by Zombie Brokers come back to life and become Zombie Stockbrokers themselves.
+
+As you'd expect from a walking corpse, they move rather slowly and are fragile. A few bullets will take them down. However, their shotguns are no joke. A few shots at close range, and you'll be history.
+
+TIPS:
+
+- Weapons with area-of-effect damage easily destroy entire groups of enemies.
+- If you're up close, hide behind walls and use ricochet grenades to take them out.
+- Of course, keep your distance. You've already seen what your shotgun can do in close combat.
+- It has the ability to regenerate.
