0cae0b215a9fffb62ca5ad3dfde8088d2443c779
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-04 03:36:31 +0300

    chaingun

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_zombiesoldier.lua
@@ -0,0 +1,31 @@
+ZOMBIE MACHINEGUN, COMMANDO
+
+
+CLASS:      Zombie Machine Gunner, Commando
+METABOLISM: Demonized Undead
+PERCEPTION: Zombie Soldier
+SIZE:       11,5 ft
+ENDURANCE:  Average
+HOSTILITY:  High
+WEAPONS:    Minigun
+REWARD:     150 FC
+THREAT:     High
+
+DESCRIPTION:
+
+After seeing how effectively demon-led zombies destroyed entire hordes of infantry with miniguns, Mental decided to incorporate these soldiers into his arsenal.
+
+The Arachnoids were too large, cumbersome, and slow to use en masse. Mental then formed an alliance with the Lord of Hell, who lent him several species, including these creatures.
+
+These zombies are former marines who underwent a complex demonic transformation ritual. Unlike regular zombies, which are simply possessed by demons, the commandos were completely remade, becoming something between human and abyssal.
+
+They still retain the remnants of their military uniform: camouflage pants and a distinctive green German helmet.
+
+The best way to deal with them is to NOT rush headlong into their fire.
+
+TIPS:
+
+- Take cover from their fire; you might be tempted to use the "No Cover, One on One" approach, but using that tactic will more likely result in you ending up in a "No Cover, Dead" situation.
+- Their minigun is inaccurate at long range; the further away you are, the better.
+- Sniping from a distance is a great way to deal with them.
+- Flare gun shots to the head are a quick fix.
