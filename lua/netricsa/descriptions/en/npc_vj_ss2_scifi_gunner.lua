2c25163f1f8f0b1c7e4e7347fb9964a1b0fbf984
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-06 20:56:23 +0300

    Gunner

--- lua/netricsa/descriptions/en/npc_vj_ss2_scifi_gunner.lua
+++ lua/netricsa/descriptions/en/npc_vj_ss2_scifi_gunner.lua
@@ -1,14 +1,14 @@
-ORC SOLDIER, GRUNT FROM THE ORAZIO
+ORC SOLDIER, GUNNER FROM THE ORAZIO
 
 
-CLASS:      Orc Soldier, Grunt
+CLASS:      Orc Soldier, Gunner
 METABOLISM: Mammal
 PERCEPTION: Primitive warrior race, former Zorgs
 HEIGHT:     6,3 ft
 ENDURANCE:  Very low
 HOSTILITY:  Very low
-WEAPONS:    Laser Blaster V2
-REWARD:     10 FC
+WEAPONS:    Rocket Laucher GVR-57 MK.4
+REWARD:     20 FC
 THREAT:     Low
 
 DESCRIPTION:
@@ -17,12 +17,9 @@ This is a clear example of the saying "they wanted the best, but it turned out a
 
 The drug was immediately banned, and the experimenters, along with all the victims, were exiled to the secluded planet of Orazio. There, the exiles made good use of their time, founding the Altani mining corporation and began mining ore, eventually falling into Mental's hands.
 
-Grunts are the lowest rung of the orc hierarchy. Their armor is rusty.
-
-The main difference between orcs and other Mental soldiers is that they know how to use technology. And they use it just as well as you.
+Gunners are Orcs who loved to blow up everything in their class; as a result, they were issued rocket launchers GVR-57 MK.4, and now serve as Mental's primary elite anti-tank infantry.
 
 TIPS:
 
-- Unlike most of Mental's minions, they will try to keep a distance when close to you.
-- They are a powerful Mental unit, as they can use vehicles just as well as you.
-- If an orc gets into a car, smash it.
+- Unlike most of Mental's minions, they will try to keep their distance when close to you.
+- You can dodge their missiles by moving to the side, or shoot them down.
