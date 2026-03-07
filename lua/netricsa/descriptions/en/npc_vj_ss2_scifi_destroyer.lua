806a9ecc27e933751f3cd1498d63c58db05f6a00
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-07 20:49:13 +0300

    orc destroyer

--- lua/netricsa/descriptions/en/npc_vj_ss2_scifi_destroyer.lua
+++ lua/netricsa/descriptions/en/npc_vj_ss2_scifi_destroyer.lua
@@ -1,14 +1,14 @@
-ORC SOLDIER, Destroyer FROM THE ORAZIO
+ORC SOLDIER, DESTROYER FROM THE ORAZIO
 
 
 CLASS:      Orc Soldier, Destroyer
 METABOLISM: Mammal
 PERCEPTION: Primitive warrior race, former Zorgs
 HEIGHT:     6,3 ft
-ENDURANCE:  Very low
-HOSTILITY:  Very low
-WEAPONS:    Laser Blaster V2
-REWARD:     10 FC
+ENDURANCE:  Low
+HOSTILITY:  Low
+WEAPONS:    Plasma Cannon
+REWARD:     20 FC
 THREAT:     Low
 
 DESCRIPTION:
@@ -17,12 +17,9 @@ This is a clear example of the saying "they wanted the best, but it turned out a
 
 The drug was immediately banned, and the experimenters, along with all the victims, were exiled to the secluded planet of Orazio. There, the exiles made good use of their time, founding the Altani mining corporation and began mining ore, eventually falling into Mental's hands.
 
-Destroyers are the lowest rung of the orc hierarchy. Their armor is rusty.
-
-The main difference between orcs and other Mental soldiers is that they know how to use technology. And they use it just as well as you.
+Destroyers are equipped with plasma cannons that fire energy blasts with primitive homing capabilities. Their primary role in Mental's Horde is to provide firepower support for their comrades.
 
 TIPS:
 
 - Unlike most of Mental's minions, they will try to keep a distance when close to you.
-- They are a powerful Mental unit, as they can use vehicles just as well as you.
-- If an orc gets into a car, smash it.
+- Their projectiles have minor homing capabilities - stay away from them.
