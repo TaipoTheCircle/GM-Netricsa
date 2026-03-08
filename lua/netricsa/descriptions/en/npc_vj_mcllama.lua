0c429c8510997c354aca1948aee38463e3677664
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-08 04:07:48 +0300

    Llamas and zobiepigman

---
+++ lua/netricsa/descriptions/en/npc_vj_mcllama.lua
@@ -0,0 +1,31 @@
+LLAMA FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:       Llama
+METABOLISM:  Mammal, Even-toed Ungulate
+PERCEPTION:  Neutral herd animal
+SIZE:        6,3 ft
+ENDURANCE:   Average
+HOSTILITY:   Conditional
+WEAPONS:     Spit
+REWARD:      120 FC
+THREAT:      Minimal
+
+DESCRIPTION:
+
+In the wild, llamas are found in savannas and windswept hills.
+
+Llamas are neutral creatures by nature. They don't attack, but if struck, the offended camel turns and spits directly in the attacker's face. The spit flies like a projectile, deals one point of damage, and doesn't catch fire. Sometimes a llama misses and hits a fellow llama, and then squabbles break out within the pack to determine who owes whom.
+
+Llamas have a particular hatred for wolves. Upon seeing a wild dog, a llama spits without warning, regardless of whether it's been touched or not. Wolves, in turn, fear strong llamas and always run away from them.
+
+Llamas can be tamed. To do this, mount them repeatedly until they stop throwing you.
+
+A tamed llama is an ideal pack animal.
+
+Besides a chest, you can decorate a llama with a rug.
+
+TIPS:
+
+- Don't hit the llamas first. The damage from the spit is negligible, but it leaves a bad taste in the mouth.
+- If you encounter a traveling merchant with llamas, you can take the llamas for yourself by untying the leash.
---
+++ lua/netricsa/descriptions/en/npc_vj_mczombiepigmancrossbow.lua
@@ -0,0 +1,32 @@
+ZOMBIFIED PIGLIN CROSSBOWMAN FROM THE MINECRAFT NETHER
+
+
+CLASS:       Zombified Piglin Crossbowman
+METABOLISM:  Undead, pig-like humanoid
+PERCEPTION:  Neutral Nether mob
+SIZE:        6,3 ft
+ENDURANCE:   Average
+HOSTILITY:   Conditional
+WEAPONS:     Golden Sword
+REWARD:      350 FC
+THREAT:      Average
+
+DESCRIPTION:
+
+A bipedal, zombified pig-like carcass, clad in scraps of once-golden armor.
+
+This is what regular Piglin Crossbowmans turn into if they remain in the Overworld or the End for more than 15 seconds. They used to be called zombie piglins, but that name was later changed.
+
+Unlike their living relatives, these piglins don't care about gold. They don't care about anything as long as they're left alone.
+
+They can also emerge from regular pigs if they're struck by lightning. Or directly from Nether portals in the Overworld-the larger the portal, the more often.
+
+Zombified piglins are immune to fire and lava-they burn but don't combust. However, they drown in water, as befits undead.
+
+They heal from damage potions and take damage from healing.
+
+TIPS:
+
+- Never strike first unless you're ready to hit everyone. One hit and the whole pack is yours.
+- They shoot quickly and painfully.
+- They're helpless in water.
