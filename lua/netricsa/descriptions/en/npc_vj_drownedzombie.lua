bf7531519e8cfa403f4e3a492b0216e5b1211c61
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-19 19:31:22 +0300

    drowned to someone fishy (lol)

---
+++ lua/netricsa/descriptions/en/npc_vj_drownedzombie.lua
@@ -0,0 +1,39 @@
+DROWNED FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:      Drowned
+METABOLISM: Aquatic Undead
+PERCEPTION: Neutral-hostile mob
+SIZE:       6,2 ft
+ENDURANCE:  High
+HOSTILITY:  Variable
+WEAPONS:    Hands, trident
+REWARD:     150 FC
+DAMNATION:  High
+
+DESCRIPTION:
+
+This is the result of a normal zombie's unfortunate bathing. If a zombie spends more than 30 seconds in water, it begins to shake, bubble, and after 15 seconds, transforms into this underwater creature. The process is irreversible, so if you see a shaking zombie in the water, prepare to encounter a Drowned Man.
+
+Dressed in tattered rags, sometimes holding a fishing rod, trident, or nautilus shell. Drowned creatures inhabit oceans, rivers, and swamps. They are most often found at the bottom of a body of water, wandering aimlessly, periodically gurgling and releasing air bubbles.
+
+Drowned creature behavior depends on the time of day and location. During the day, on land, Drowned creatures are neutral-they won't attack even if struck first. However, if a victim is even partially submerged in water, the entire pack instantly becomes hostile. At night or during thunderstorms, Drowned creatures emerge to hunt and pursue victims even on land, like regular zombies.
+
+Drowned creatures attack iron golems, small turtle hatchlings, and trample turtle eggs (even through narrow passages). If they kill a villager, they have up to a 100% chance of turning into a zombie villager. They are also immune to attacks from Guardians and Elder Guardians.
+
+Like all undead, Drowned burn in the sun if they find themselves on land during the day. But unlike zombies, they immediately seek out the nearest body of water to hide. They can swim in water with impressive speed.
+
+Drowned sometimes appear riding a chicken, creating a rare Drowned Rider.
+
+When dying, a Drowned may drop rotten flesh, copper ingots, and, if lucky, a trident or nautilus shell.
+
+TIPS:
+
+- Drowned on land are safe during the day - you can simply walk past them.
+- If you hear gurgling and see a shaking zombie in the water, move away; a Drowned will soon be there.
+- The main threat is a Drowned with a trident. Don't stand in the open; use cover.
+- During daylight combat, a Drowned on land will burn in the sun - you can simply wait.
+- If you're fighting in the water, try to get to dry land quickly-the Drowned don't like to pursue you too far.
+- A shield protects against trident throws.
+- To guarantee a trident, use the "Loot III" enchantment and only kill Drowned who are holding a trident.
+- Drowned don't live in warm oceans-they're safe there.
