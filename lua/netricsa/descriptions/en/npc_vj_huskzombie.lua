47cd13dd4d53fd04277df02f959d4153235fe853
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-20 20:45:37 +0300

    husk

---
+++ lua/netricsa/descriptions/en/npc_vj_huskzombie.lua
@@ -0,0 +1,33 @@
+HUSK FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:      Husk
+METABOLISM: Desert Undead
+PERCEPTION: Hostile mob
+SIZE:       6,2 ft
+ENDURANCE:  High
+HOSTILITY:  High
+WEAPONS:    Hands, "Hunger" effect
+REWARD:     160 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+This is a zombie that has been exposed to the sun for too long and is now immune to harm. Unlike their regular counterparts, these creatures don't burn in the desert during the day because their dried skin has long since lost its sensitivity to ultraviolet light. Visually, a Husk is 2 cm taller than a normal zombie, preventing it from squeezing through two-block gaps.
+
+They terrorize deserts, savannas, and other hot biomes. They roam in groups of 2-4 individuals when light levels are below 7, exclusively outdoors.
+
+The Husk's main characteristic is its "Hunger" attack. If it's empty-handed (no weapons), its touch drains your hunger faster than sand drains water. The duration of the effect depends on the difficulty and regional difficulty: from 7 to 30 seconds. A shield blocks both damage and hunger, so keep it ready.
+
+Otherwise, the Husk behaves like a normal zombie: it stalks victims, villagers, iron golems, and turtle eggs from a distance of 40 meters. It can pick up objects, break down doors, and set ambushes. If a Husk enters water, it will begin to shake after 30 seconds, and after another 15 seconds, it will transform into a normal zombie. This process is irreversible, even if you pull it out onto dry land. This zombie can then become a drowned zombie if it continues to sit in the water.
+
+Upon death, a husk leaves behind rotten flesh, and occasionally a carrot, potato, or iron ingot. A baked potato drops if the mob is burned. If the husk was equipped, it drops with a 100% chance. On Halloween, they can spawn with a pumpkin on their head.
+
+TIPS:
+
+- Husks don't burn in the sun-don't expect them to die by morning.
+- Hitting a husk inflicts hunger-always have a supply of food or a shield.
+- Chase a husk into water-after 45 seconds, it will transform into a regular zombie, which is easier to control.
+- Hatchlings are fast and can mount anything-don't let them surround you.
+- For a guaranteed drop, use the "Loot III" enchantment.
+- Husks are slightly taller than zombies-they sometimes get stuck in gaps where regular undead can pass.
