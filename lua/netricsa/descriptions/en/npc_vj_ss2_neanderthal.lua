c81a0afd5c0fa233c8c37d73b7d36cd7dd0872c2
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-21 20:26:49 +0300

    BRUTE PRIMITIVE

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_neanderthal.lua
@@ -0,0 +1,27 @@
+PRIMITIVE BRUTE FROM M'DIGBO
+
+
+CLASS:      Primitive Brute
+METABOLISM: Mammal primitive being
+PERCEPTION: Primitive being
+SIZE:       6,8 ft
+ENDURANCE:  Medium
+HOSTILITY:  Medium
+WEAPONS:    Spiked club, wooden shield
+REWARD:     150 FC
+THREAT:     low
+
+DESCRIPTION:
+
+M'Digbo is dotted with countless primitive tribes, all of which display highly territorial and aggressive behavior. They attack almost everyone on impulse and are no smarter than a bunch of bricks.
+
+They are a humanoid race led by the mysterious "Mommy".
+
+When Mental's forces colonized M'Digbo, the primitives launched attacks against them. They all died. After that, they started worshipping Mental as a god of war and pledged their allegience to him. This was done out of their fear and respect for Mental, as well as the stylistic leopardic panties that Sirius Fashion Inc provided them with.
+
+Their most powerful weapon is a simple spiked club, though some also carry wooden shields custom-made from the planet Chi-Fang. Tactics are non-existent among them, so expect them to rush after you while angrily swinging their clubs.
+
+TIPS:
+
+- They move slowly and can be taken out relatively easily, they should be a low priority among other foes.
+- Don't underestimate their clubs, a big smack on the head and you'll be left with more than just a headache. Keep your distance.
