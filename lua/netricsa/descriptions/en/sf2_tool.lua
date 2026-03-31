61e884061090dd25133b73fd8cc97d7e32a6a072
TaipoTheCircle <tails321supsen@gmail.com>
2026-03-31 05:46:52 +0300

    storm fox 2 tool

---
+++ lua/netricsa/descriptions/en/sf2_tool.lua
@@ -0,0 +1,21 @@
+STORMFOX 2 TOOL
+
+
+WEAPON: StormFox 2 Tool
+AMMO:   Environmental Energy / Weather and Light Pulses
+ROF:    ~2-4 actions per second
+
+DESCRIPTION:
+
+A specialized environmental control device that allows you to alter weather and lighting conditions in a given area.
+
+The first mode is a surface editor. During snowfall, the user can specify zones where snow will appear or disappear completely. This allows you to clear snow or, conversely, leave the enemy uncloaked.
+
+The second mode is a light editor. The device allows you to place light sources and activate lamps in a selected area, instantly changing visibility on the battlefield.
+
+NOTES:
+
+- Controls the presence of snow: adds or removes cover (only during snowfall).
+- Allows you to place light sources and activate lighting.
+- Effective for territory control and tactical advantage.
+- Removing snow deprives camouflaged enemies of snow cover, while adding snow, on the contrary, conceals their positions.
