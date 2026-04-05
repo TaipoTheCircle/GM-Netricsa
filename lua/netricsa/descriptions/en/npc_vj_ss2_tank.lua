273ee1a2de23e54913a4609a21b9e82911ce0d62
TaipoTheCircle <tails321supsen@gmail.com>
2026-04-06 02:35:49 +0300

    bulltank

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_tank.lua
@@ -0,0 +1,26 @@
+BIOMECHANOID BULLTANK, SERGEANT
+
+
+CLASS:      Biomechanoid BullTank, Sergeant
+METABOLISM: Endothermous Cyborg
+PERCEPTION: Cybernetically Enhanced
+SIZE:       10 ft
+ENDURANCE:  Medium
+HOSTILITY:  High
+WEAPONS:    Pulse Cannons
+REWARD:     200 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+Following the successful creation of Biomechanoid Walkers, Mental tasked its scientists with finding a way to convert existing troops into biomechanoids, so that they may continue to serve Mental even after suffering grievous wounds. The result was the Biomechanoid Tank.
+
+Bullsoldiers that have been incapacitated in battle have their damaged body parts amputated, and cybernetic augmentations are surgically implanted in their place. The body is then connected to a tank chassis capable of traversing most forms of terrain, making them highly versatile.
+
+Sergeants are designed to suppress enemy infantry on the battlefield. Their hands have been replaced with pulse cannons that fire bursts of laser beams.
+
+TIPS:
+
+- Dodge their laser beams by side stepping.
+- The closer they are, the higher the threat they pose - keep your distance.
+- Use the Sniper Rifle or explosives to take them out quickly.
