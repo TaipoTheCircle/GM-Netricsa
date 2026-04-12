3ff602e4e5e8875e7d6607bacb7f07954de94ec2
TaipoTheCircle <tails321supsen@gmail.com>
2026-04-13 01:14:53 +0300

    MC FINAL (VANILA)

---
+++ lua/netricsa/descriptions/en/npc_vj_mcravager.lua
@@ -0,0 +1,36 @@
+RAVAGER FROM THE MINECRAFT OVERWORLD
+
+
+CLASS:      Ravage
+METABOLISM: Illager Beast
+PERCEPTION: Siege Weapon / Heavy Shock Unit
+SIZE:       7,2 ft
+ENDURANCE:  Extreme
+HOSTILITY:  Extreme
+WEAPONS:    Headbutt, Roar, Block Breaker
+REWARD:     2000 FC
+THREAT:     Extreme
+
+DESCRIPTION:
+
+This is the living battering ram and siege weapon of the Illager army. It is a gigantic beast, resembling a hybrid of a bull, a rhinoceros, and a mythical manticore, which looks as if it feeds solely on fear and village walls.
+
+The Ravage is a massive, gray-brown hulk with enormous horns and a saddle embedded in its withers.
+
+The Ravager doesn't just appear. It appears exclusively during Village Raids, starting with the 3rd wave. In the 5th and 7th waves, it is joined by riders: Crossbow-wielding Pillagers or Mighty Evokers, transforming the beast into a mobile combat platform.
+
+The Ravager has three main deadly mechanics:
+
+Battering Ram (Charge): It charges and slams its head into the ground, dealing massive damage and knocking the target back 5 blocks.
+
+Terrain Destruction: The Ravager demolishes everything in its path. It easily destroys foliage, crops (wheat, carrots, potatoes, beets), and even sugarcane, mushrooms, and turtle eggs.
+
+Roar: If its attack is blocked with a shield, there's a chance to stun it, but when it wakes up, it unleashes a Roar that deals damage and knocks back everyone around it.
+
+TIPS:
+
+- When the Ravager attacks with its horns, use your shield. You'll block the damage, and the beast has a 50% chance of being stunned and defenseless for a few seconds.
+- If you don't dodge the ramming charge, you'll be thrown back ten blocks, which in the chaos of battle often leads to falling off a cliff or being surrounded.
+- If the Ravager is controlled by an archer or mage, kill the rider first.
+- After exiting the stun, it roars. Move away, otherwise it will knock you back and deal damage.
+- Unlike the shield, arrow and crossbow damage is ineffective against it due to its armor, but long-range fire helps control the distance.
---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_lizard_rider.lua
@@ -0,0 +1,23 @@
+MARSH FIEND FROM RIGIL KENTAURUS
+
+
+CLASS:      Marsh Fiend
+METABOLISM: Amphibian reptile
+PERCEPTION: Wild animal
+SIZE:       3 ft
+ENDURANCE:  Low
+HOSTILITY:  High
+WEAPONS:    Fangs, claws
+REWARD:     50 FC
+THREAT:     Low to medium (depending on number)
+
+DESCRIPTION:
+
+Marsh-Fiends originate from the marsh-filled wasteland worlds of Rigil Kentaurus. They typically lurk near water, waiting for their prey to come close and drink before they strike.
+Mental assimilated these creatueres into its Horde alongside their Marsh-Hopper cousins. Due to the extreme rate at which they breed, Mental can afford to send swarm after swarm against its foes.
+Their only methods of attack are dashing forward and biting. So long as you keep your distance and gun them down, you'll be fine.
+
+TIPS:
+
+- You cannot outrun them, they'll eventually catch up with you. Shoot them down while they're afar.
+- Use your shotguns to deal with small groups of them, and explosives to take care of larger swarms.
