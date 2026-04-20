f8b9dbf46f51298c56538fe00bf53de21c3d0219
TaipoTheCircle <tails321supsen@gmail.com>
2026-04-20 04:49:50 +0300

    shaman update

---
+++ lua/netricsa/descriptions/en/npc_vj_ssc_eyeman_shaman_female_invisible.lua
@@ -0,0 +1,28 @@
+PRIMITIVE GNAAR, FEMALE SHAMAN
+
+
+CLASS:      Primitive Gnaar, Female Shaman
+METABOLISM: Mammal
+PERCEPTION: Trained breeding war animal
+SIZE:       5 ft
+ENDURANCE:  Low
+HOSTILITY:  Low
+WEAPONS:    Bites, Punches
+REWARD:     1200 FC
+THREAT:     Medium
+
+DESCRIPTION:
+
+A small bipedal animal with no head, one eye and a large mouth. Originating from Sirius, and trained by Mental to attack opposing forces. Female Shamans have a Skull pattern on their backs, and are larger and stronger than males.
+
+The apex of the primitive subspecies' social pyramid. Unlike the males, who merely serve and wear leopard-skin pants, absolutely all females in the tribe become shamans. There's no selection, no testing-she's born a female, raised, and becomes a shaman by default. In the primitive Gnaar tribes, this isn't a privilege, but a duty.
+
+In appearance, the female shaman is radically different from the males. Her skin is green, a swampy shade, making her less noticeable. But the main difference is the huge red skull painted on her back. A ritual symbol visible from afar, it serves as a reminder: this individual is in charge, and arguing with her is risky.
+
+The shamans don't simply govern the tribe. They direct the males, resolve trade and war issues, and maintain contact with Mental. When not governing, they, like the males, often trade with the tropical Gnaar species, exchanging resources and, perhaps, tattoo secrets.
+
+TIPS:
+
+- They have no range weapons, just keep away.
+- Some species can levitate.
+- Rare specimens have been reported to be invisible.
