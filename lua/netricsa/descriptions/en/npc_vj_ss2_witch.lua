fb4975a9f1f9c9bd6b4be7941d03c4bc37db3653
TaipoTheCircle <tails321supsen@gmail.com>
2026-04-23 20:50:21 +0300

    dynja

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_witch.lua
@@ -0,0 +1,29 @@
+DYNJA THE ALCHEMIST OF MAGNOR
+
+
+CLASS:      Dynja the Alchemist
+METABOLISM: Mammal, Former Beauty
+PERCEPTION: Witch
+SIZE:       4,6 ft
+ENDURANCE:  Average
+HOSTILITY:  High
+WEAPONS:    Magic Skulls, Cauldron, Hands
+REWARD:     50 FC
+THREAT:     High
+
+DESCRIPTION:
+
+She was essentially one of the most beautiful girls in the galaxy, but when she and her sister were ready to participate in Mental's beauty pageants, they turned into ugly witches. After this mishap, they were easily conscripted into Mental's army.
+
+Dynja is Yagoda's twin sister; together, they are the most famous witches on the planet Magnor. They are renowned for their sadistic nature, mastery of their art, and fondness for alcohol.
+
+She's an alchemist who mastered the art of creating cloning potions and used them to reproduce herself (self-cloning). Seeing her potential, Mental offered her a lifetime supply of vodka in exchange for her services. She happily accepted.
+
+Dynja hides on the ground, summoning magical skulls from her cauldron to stalk her enemies.
+
+TIPS:
+
+- They prefer to attack as much as possible, rather than to relocate or dodge your attacks.
+- Explosives and sniper shots can take them out easily.
+- Although a double-barrel shotgun blast can take them out in close range, it is ill-advised to approach them.
+- The skulls will chase after you, so stay on the move if you don't want to get spooked.
