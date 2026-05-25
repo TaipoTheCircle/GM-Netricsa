90db041a5137d98b101c92bc73f89b072db68818
TaipoTheCircle <tails321supsen@gmail.com>
2026-05-25 22:57:59 +0300

    Robotfloater

---
+++ lua/netricsa/descriptions/en/npc_vj_hlrdp_zombie_soldier.lua
@@ -0,0 +1,29 @@
+Y.S.M. ZOMBIE HEADCRAB, COMMON
+
+
+CLASS:      Y.S.M. Zombie Headcrab, Common
+METABOLISM: Human Host + Parasitic Headcrab Of Xen
+PERCEPTION: Zombified Entity, Former Contractor
+SIZE:       6,2 ft
+ENDURANCE:  Very High
+HOSTILITY:  Extreme
+WEAPONS:    Claws, Bite
+REWARD:     500 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+This is what the mercenaries of the private military company Y.S.M. turn into after an unfortunate encounter with a headcrab. Unlike scientists in gray coats, technicians in coveralls, or even the Delta Base guards, these guys were originally professional soldiers. They wore body armor, tactical helmets, and knew how to handle weapons. This didn't save them from infection, but it made them much more dangerous opponents in their zombified form.
+
+Their armor partially protects their bodies from gunfire, making them more resilient than regular zombies. The headcrab perched on their head looks the same as that of other variants, but its host is a trained, muscular body that doesn't fall apart from a couple of blows.
+
+They are larger than the Delta Base guards, their armor is thicker, and their blows are heavier. Even after the headcrab takes control, the former mercenary retains muscle memory: they move faster than regular zombies. Their claws pierce armor, and their bites tear out chunks of flesh.
+
+A headshot detaches the head.
+
+TIPS:
+
+- Y.S.M. zombies are more resilient than all other standard zombies. Don't skimp on ammo.
+- His armor protects his chest and shoulders. Aim for the head-it's vulnerable, like everyone else's.
+- Keep your distance-he's a bit faster than a normal zombie.
+- Don't think that the remains of his tactical gear are trophies. He doesn't need them anymore, and you certainly don't.
---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_robotfloater.lua
@@ -0,0 +1,25 @@
+FLOATER DRONE FROM THE SIRIUS
+
+
+CLASS:      Floater Drone 
+METABOLISM: Al-Controlled Aircraft
+PERCEPTION: Primitive Al
+SIZE:       2 ft
+ENDURANCE:  Very Low
+HOSTILITY:  Medium
+WEAPONS:    Lasergun
+REWARD:     200 FC
+THREAT:     Low
+
+DESCRIPTION:
+
+Are light, flying reconnaissance units designed for scouting, although they are extensively used by the Sirian police as well, typically handing out speeding tickets to reckless drivers and pilots.
+
+Floaters will circle around enemies and shoot them with a low-yield lasergun. One of them is a minimal threat, however, they will transmit a distress call to all nearby floaters, as well as the local Drone Command HQ.
+
+If you are spotted by a Floater, don't be surprised if you see the bulk of Mental's forces coming to reinforce the area.
+
+TIPS:
+
+- They can be easily dispatched with most weapons, but rapid-fire and area-damage ones work best.
+- They should be low-priority targets among other foes, as long as you are on the move and dodge their lasers.
