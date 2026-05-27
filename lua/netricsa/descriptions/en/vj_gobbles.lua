f4a8dd9bfbb4342832ad87318fb6541b442999a2
TaipoTheCircle <tails321supsen@gmail.com>
2026-05-27 20:48:47 +0300

    vj_gobbles

---
+++ lua/netricsa/descriptions/en/vj_gobbles.lua
@@ -0,0 +1,26 @@
+GOBBLES LEARNOSAUR
+
+
+CLASS:      Gobbles Learnosaur
+METABOLISM: Dinosaur, Learnosaur
+PERCEPTION: Member Of The "Support" Team
+SIZE:       5,6 ft
+ENDURANCE:  Average
+HOSTILITY:  None
+WEAPONS:    Tie, Knowledge, Trained Courage
+REWARD:     120 FC
+THREAT:     High
+
+DESCRIPTION:
+
+This is a purple dinosaur with pink eyes, a white belly, and sparse pink plates on its back. It wears a blue tie with a white "G" in the middle. It originates from the educational preschool game "Gobbles & the Learnosaurs", where it was the main character.
+
+In his home world, Gobbles and his friend Stegosaurus ventured into the villain Cromugn's cave to retrieve the stolen consonants. They succeeded, but the hero's victory led to the destruction of the world. Cromugn was devoured by a pillar of fire, Stegosaurus was crushed (cut in half), and Gobbles was saved at the last moment by Kit and Kaboodle, who recruited him to the "Support" team.
+
+Gobbles is a nervous, timid, and unprepared dinosaur who is constantly panicking. Kaboodle considers him a burden and suggests leaving him to his fate, but Kit is kind to him, offering him juice and constantly reassuring him. Gobbles tries to prove his usefulness, but this is difficult. However, behind this cowardly facade lies a sensitive personality capable of courage.
+
+Gobbles is a dinosaur willing to learn anything under the sun. He taught Flappers to read and write, and he himself learned courage from the world around him and its heroes.
+
+TIPS:
+
+- Gobbles is no fighter. His strength lies in his ability to notice details and care for his friends.
