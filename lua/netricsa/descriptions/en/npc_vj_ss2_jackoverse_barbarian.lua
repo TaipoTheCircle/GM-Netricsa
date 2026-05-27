5e45ca76e5e6784bc217051e3303c082f0dcbfaa
TaipoTheCircle <tails321supsen@gmail.com>
2026-05-27 23:04:26 +0300

    artur

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_jackoverse_barbarian.lua
@@ -0,0 +1,27 @@
+ONAN ARTHUR
+
+
+CLASS:      Onan Arthur
+METABOLISM: Mammal
+PERCEPTION: Intelligent And Strong Warrior
+SIZE:       8,9 ft
+ENDURANCE:  High
+HOSTILITY:  Conditional
+WEAPONS:    Ancient Magic Axe "Excalibur"
+REWARD:     400 FC
+THREAT:     High
+
+DESCRIPTION:
+
+A member of the Onan Librarian clan, a rare combination of knowledge keepers and warriors. Unlike many of his kind, he was no fool and demonstrated both intelligence and strength from his youth. Despite his menacing appearance, Arthur is known as one of the kindest heroes and often helps those in trouble.
+
+The culmination of his journey was the battle at the top of the Volcano, where he defeated the Great Mighty Lava Golem and seized the Ancient Magic Axe—an artifact of incredible destructive power, which in his hands serves not for destruction, but for defense. This axe is considered one of the most dangerous artifacts, but Arthur uses it wisely.
+
+Onan Arthur fought the witches, but in one clash, he was forced to retreat to save his own ass. Later, he defeated the Romans and Sanya, proving once again that his ferocity and kindness go hand in hand with his willingness to defend his world. His victory over the golem, the Romans, and Sanya made him a legend among the Onan Librarians.
+
+TIPS:
+
+- Arthur doesn't attack first those who pose no threat.
+- If you see Arthur retreating, don't assume it's cowardice. He might just be saving someone or his own ass.
+- Don't try to steal his axe. It's useless and deadly.
+- If you're in trouble and Arthur's by your side, consider yourself lucky.
