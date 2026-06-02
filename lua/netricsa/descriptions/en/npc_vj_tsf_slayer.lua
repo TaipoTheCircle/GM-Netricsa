c1a3b44bf3615ea3b74318802fcd814d6b054272
TaipoTheCircle <tails321supsen@gmail.com>
2026-06-02 16:26:46 +0300

    slayers update

--- lua/netricsa/descriptions/en/npc_vj_tsf_slayer.lua
+++ lua/netricsa/descriptions/en/npc_vj_tsf_slayer.lua
@@ -1,4 +1,4 @@
-SLAYER FROM CARNATE ISLAND
+SLAYER FROM THE CARNATE ISLAND
 
 
 CLASS:      Slayer Of Carnate Island
@@ -7,7 +7,7 @@ PERCEPTION: Malefactor / Horror Entity
 SIZE:       ~6 ft
 ENDURANCE:  High 
 HOSTILITY:  Extreme
-WEAPONS:    Blade limbs (arms and legs are long, sharp blades)
+WEAPONS:    Blade Limbs (Arms And Legs Are Long, Sharp Blades)
 REWARD:     1000 FC 
 THREAT:     Very High
 
@@ -15,7 +15,13 @@ DESCRIPTION:
 
 The Slayer is one of the most iconic and terrifying enemies on the island. Created as a representation of execution by beheading, its body is gruesomely melded: its head is detached and held above its body by flesh and steel apparati, while its limbs have transformed into long, lethal blades. 
 
-Slayers move with unnatural agility, able to skulk along walls and ceilings, drop down on victims, or slash at them from “above.” They are often heard before seen - their blade-limbs scrape and whisper, giving away their approach.
+Slayers move with unnatural agility, able to skulk along walls and ceilings, drop down on victims, or slash at them from "above". They are often heard before seen - their blade-limbs scrape and whisper, giving away their approach.
+
+I first witnessed these creatures jumping out of the ground itself, their heads are detached from their torso, held aloft by hideous contraptions. their limbs have been replaced by blades of the sharpest steel.
+
+To my eyes, they appear to be a manifestation of decapitation, yet it seems improbable anyone ever had their head chopped off in Abbott. I suppose on carnage anything is possible.
+
+I have dubbed these monstrosities slavers.
 
 TIPS:
 
