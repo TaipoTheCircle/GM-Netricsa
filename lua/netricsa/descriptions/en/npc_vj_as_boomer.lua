970be9b7cf5a35131cbb415580b7b4b7387decfc
TaipoTheCircle <tails321supsen@gmail.com>
2026-06-22 01:15:42 +0300

    boomer

---
+++ lua/netricsa/descriptions/en/npc_vj_as_boomer.lua
@@ -0,0 +1,31 @@
+SWARM BOOMER
+
+
+CLASS:      Swarm Boomer
+METABOLISM: Xenobiological
+PERCEPTION: Living Explosives
+SIZE:       7,9 ft
+ENDURANCE:  Average
+HOSTILITY:  Absolute
+WEAPONS:    Self-Detonating, Explosive Capsules
+REWARD:     600 FC
+THREAT:     High
+
+DESCRIPTION:
+
+The Boomer is a specialized Swarm specimen, whose entire biology is dedicated to one task: delivering the maximum amount of explosives as close to the enemy as possible.
+
+When at rest, the Boomer appears relatively harmless for a Swarm specimen of its size. However, upon detecting prey, its internal organs begin to rapidly produce unstable chemical compounds. The creature's body swells, turning into a living reservoir of explosive biomass.
+
+To close the distance, Boomer can perform swift, sliding attacks, knocking opponents out of position and blocking passages with its own body. Upon reaching close range, the organism detonates, destroying itself and everything nearby.
+
+The secondary explosive capsules pose a particular danger. After the death of a bloated Boomer, they scatter around the explosion site and detonate a few seconds later. Therefore, even successfully destroying the creature does not always mean the end of the threat.
+
+According to xenobiologists, Boomer is one of the most striking examples of how far evolution can go when the instinct for self-preservation is completely abandoned.
+
+TIPS:
+
+- Destroy Boomers before they bloat.
+- After the explosion, leave the danger zone immediately.
+- Do not allow them to approach the group.
+- If the creature looks like it's about to burst, it's best not to test the literalness of this statement.
