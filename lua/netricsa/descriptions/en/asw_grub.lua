33d8839ae3f983e016d0e343dea3741ddfff0ecc
TaipoTheCircle <tails321supsen@gmail.com>
2026-07-10 20:01:50 +0300

    grub fix

---
+++ lua/netricsa/descriptions/en/asw_grub.lua
@@ -0,0 +1,30 @@
+SWARM GRUB
+
+
+CLASS:      Swarm Grub
+METABOLISM: Xenobiological
+PERCEPTION: Immature Life Form
+SIZE:       0,98 ft
+ENDURANCE:  Very low
+HOSTILITY:  None
+WEAPONS:    None
+REWARD:     10 FC
+THREAT:     Minor
+
+DESCRIPTION:
+
+A Grub is one of the early stages of Swarm organisms' development. These creatures most often emerge from specialized brood sacs that open upon detection of prey or exposure to high temperatures.
+
+Unlike most Swarm members, Grub are incapable of attacking opponents and are virtually defenseless. Their primitive nervous system allows them only to react to movement and follow large living objects.
+
+Despite the lack of a direct threat, the presence of Grub can serve as an important indicator of nearby Swarm activity. The discovery of brood sacs usually indicates the presence of more dangerous organisms in the area.
+
+As the Grub develop, they undergo further stages of metamorphosis, gradually transforming into fully fledged representatives of their species. The precise life forms that emerge from this process are still a subject of debate among xenobiologists.
+
+Most fighters first experience compassion for members of the Swarm upon encountering the Grub. This typically continues until they discover what they grow into.
+
+TIPS:
+
+- Don't be distracted by the Grub during combat.
+- Their presence often indicates that something far more dangerous is nearby.
+- A small, harmless organism today can become a major problem tomorrow.
