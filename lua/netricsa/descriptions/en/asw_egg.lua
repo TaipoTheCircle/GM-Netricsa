f120847d9b20c40615acd71eaab1f3641dd0c6b9
TaipoTheCircle <tails321supsen@gmail.com>
2026-07-10 04:49:08 +0300

    parasite egg

---
+++ lua/netricsa/descriptions/en/asw_egg.lua
@@ -0,0 +1,29 @@
+SWARM PARASITE EGG
+
+
+CLASS:      Swarm Parasite Egg
+METABOLISM: Xenobiological
+PERCEPTION: Parasite Incubator
+SIZE:       6,5 ft
+ENDURANCE:  Very Low
+HOSTILITY:  Medium
+WEAPONS:    Parasite Release
+REWARD:     50 FC
+THREAT:     High
+
+DESCRIPTION:
+
+The Swarm Parasite Egg is a specialized incubation chamber designed for the development of parasites. It is covered with a thin elastic membrane, inside of which there is a thick nutrient fluid necessary for the maturation of the embryo.
+
+In a calm state, the eggs remain motionless, waiting for living organisms to emerge. When potential prey approaches, the shell instantly opens, releasing the parasite, which immediately attacks the nearest target. Once hatched, the egg quickly loses viability and is no longer capable of producing new eggs.
+
+Eggs are usually located in groups inside hives, ventilation shafts, tunnels and other damp areas where favorable conditions are maintained for the development of offspring. They can often be found next to larval cocoons and other incubators, forming full-fledged swarm breeding areas.
+
+Despite their immobility, a large number of eggs can turn a safe passage into a deadly trap. Even if the adults are destroyed, the remaining clutches allow the Swarm to quickly restore its numbers.
+
+TIPS:
+
+- Destroy the eggs before you get close to them.
+- Fire allows you to safely clear contaminated areas with a large number of clutches.
+- After opening the shell, focus the fire on the hatched parasite.
+- If there are several eggs nearby, try not to activate them at the same time.
