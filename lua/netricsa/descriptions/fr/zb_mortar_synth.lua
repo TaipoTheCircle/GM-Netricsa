5462b34510d20a8cda09aba49ac562783ae9df13
TaipoTheCircle <tails321supsen@gmail.com>
2026-07-12 07:23:28 +0300

    mortar

---
+++ lua/netricsa/descriptions/fr/zb_mortar_synth.lua
@@ -0,0 +1,26 @@
+COMBINEZ LE SYNTHÉ DE MORTIER
+
+
+CLASS: Combiner un synthétiseur de mortier
+METABOLISM: Organisme d'artillerie biomécanique (alimentation à noyau acide)
+PERCEPTION: Synthé d'artillerie
+SIZE: ~ 18 pieds de haut (avec le canon levé)
+ENDURANCE: Élevé (carapace blindée, mais moins mobile)
+HOSTILITY: Extrême
+WEAPONS: Canon à mortier organique lourd, modules de bombardement électrique
+REWARD: 1600 FC
+THREAT: Catastrophique (unité de classe siège)
+
+DESCRIPTION: 
+
+Le Combine Mortar Synth est un organisme d'artillerie conçu par le Combine pour anéantir à distance les bastions de la résistance. Sa forme grotesque ressemble à un insecte biomécanique fusionné avec un canon de siège : la colonne vertébrale se courbe vers le haut pour lancer des capsules explosives sur de grandes distances.
+
+Each mortar shot is devastating: Les modules de bombardement acides éclatent en nuages ​​corrosifs lors de l'impact, brûlant les armures, les fortifications et la chair. Bien que dévastateurs à distance, les Mortar Synths se déplacent lentement et moins efficaces au corps à corps, s'appuyant sur leur masse massive pour écraser tout ennemi qui s'approche trop près.
+
+Ils sont souvent déployés sur le champ de bataille en tandem avec des Striders et des Crab Synths, fournissant un tir de suppression pendant que les unités d'assaut plus lourdes avancent. Des bases entières de résistance ont été réduites à des cratères fumants après des bombardements prolongés de Mortar Synth.
+
+TIPS: 
+
+- Ne restez jamais à découvert : leur bombardement est très précis à distance.
+- Utiliser des tactiques de délit de fuite ; rapprochez-vous rapidement pour éviter des bombardements prolongés.
+- Les RPG et les explosifs dirigés vers la colonne vertébrale du mortier sont les contre-attaques les plus efficaces.
