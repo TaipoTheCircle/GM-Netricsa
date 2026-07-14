ad98fa0f14771402c80eedd8d2d6f57197ebbd43
TaipoTheCircle <tails321supsen@gmail.com>
2026-07-14 19:34:32 +0300

    edensys

---
+++ lua/netricsa/descriptions/en/npc_vj_severedsteel_edensys_soldier.lua
@@ -0,0 +1,31 @@
+EDENSYS BIOTECH SOLDIER
+
+
+CLASS:      EdenSys BioTech Soldier
+METABOLISM: Human Clone, Male
+PERCEPTION: EdenSys Corporate Security Force
+SIZE:       5,9 ft
+ENDURANCE:  Average
+HOSTILITY:  Extreme
+WEAPONS:    Various Small Arms
+REWARD:     600 FC
+THREAT:     Average
+
+DESCRIPTION:
+
+These soldiers form the core combat force of the EdenSys BioTech Corporation's security force. Most are clones, bred and trained exclusively for combat, facility security, and the suppression of any internal or external threats.
+
+Their equipment is designed for use in extreme cold conditions. Insulated uniforms, thick masks, and visors allow them to operate in arctic environments while maintaining high mobility and effectiveness even in extremely low temperatures.
+
+Warfare depends on the assigned mission. Soldiers are capable of using a wide range of weapons: assault rifles, shotguns, submachine guns, rifles, machine guns, and other modern corporate weaponry. Thanks to constant training, they confidently handle virtually any type of firearm.
+
+EdenSys's training is considered extremely rigorous, but most soldiers learn one thing best from the entire course: an endless supply of obscenities. In combat, they constantly shout curses, threats, and insults, which has long become a hallmark of the corporate army.
+
+Despite this, they should not be underestimated. They act in an organized manner, quickly occupying advantageous positions, actively exploiting cover, and delivering concentrated fire, posing a serious threat even to well-trained opponents.
+
+TIPS:
+
+- The weapons used are unknown in advance, so assess the threat immediately upon spotting the enemy.
+- Soldiers prefer to fight from cover and rarely remain in the open.
+- Their arctic gear doesn't make them any more protected than ordinary people.
+- Don't be distracted by their constant cursing: they shoot much better than they talk.
---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_jackoverse_mental.lua
@@ -0,0 +1,35 @@
+MENTAL (TAH-UM)
+
+
+CLASS:      Mental (Tah-Um)
+METABOLISM: Immortal God
+PERCEPTION: Last of the Hum-Tah Race
+SIZE:       Unknown
+ENDURANCE:  Virtually Unlimited
+HOSTILITY:  Absolute
+WEAPONS:    Psionic Abilities, Ancient Magic, Hum-Tah Technology, Countless Armies
+REWARD:     Peace
+THREAT:     Maximum
+
+DESCRIPTION:
+
+Mental, also known by his true name, Tah-Um, is considered the greatest threat to intelligent life in the known galaxy. He is the last known representative of the ancient race of immortal, godlike beings, the Hum-Tah, who once possessed knowledge far superior to the modern technology of most civilizations.
+
+Mental is estimated to be millions of years old. During this time, he has conquered or destroyed countless worlds, wiping entire civilizations from the face of the universe. Captured peoples are either completely exterminated or become part of his endless army through biological, mechanical, or genetic modification.
+
+Most of Mental's known armies are composed not of volunteers, but of enslaved beings. Many life forms have been artificially modified specifically for war. Captives are often subjected to cruel experiments or executions, after which their bodies are used to create new combat units. This is how numerous headless soldiers, kamikazes, and other members of his forces were created.
+
+Despite his virtually unlimited power, Mental himself rarely appears on the battlefield in person. Instead, he directs the war through generals, commanders, and numerous governors, coordinating the actions of his armies with colossal psionic abilities. His observable form is merely a gigantic dark projection, while Tah-Um's true appearance remains unknown.
+
+In addition to his incredible intellect, Mental possesses the ancient magic of Hum-Tah, the technology of a vanished civilization, and psionic abilities that allow him to influence the minds of others, control vast armies, and coordinate invasions across multiple star systems.
+
+Over millennia of warfare, the only enemy who has repeatedly thwarted his plans was "Serious" Sam Stone (You). It was this inability to completely eliminate a single individual that forced Mental to resort to the most radical methods of warfare, including the destruction of entire planets.
+
+Despite all the information gathered, the origins of Hum-Tah, the limits of Tah-Um's powers, and his true location remain unknown.
+
+TIPS:
+
+- If you encounter Mental in person, events have taken a very unfavorable turn.
+- Don't underestimate the enemy just because he rarely engages in battle personally.
+- Dodge his attacks.
+- He can't throw the Moon on himself; let your imagination run wild!
