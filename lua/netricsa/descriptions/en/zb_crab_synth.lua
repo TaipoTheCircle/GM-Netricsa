949632ffa5041a3f7fc9b179bf149098200a2f47
TaipoTheCircle <tails321supsen@gmail.com>
2026-08-11 15:51:11 +0300

    Crab Synth

---
+++ lua/netricsa/descriptions/en/zb_crab_synth.lua
@@ -0,0 +1,30 @@
+COMBINE CRAB-SYNTH
+
+
+CLASS:      Combine Crab-synth
+METABOLISM: Biomechanical Synthetic
+PERCEPTION: Heavy Assault Unit
+SIZE:       7,2 ft
+ENDURANCE:  High
+HOSTILITY:  Extreme
+WEAPONS:    Retractable Minigun, Claws, Ram
+REWARD:     800 FC
+THREAT:     Very High
+
+DESCRIPTION:
+
+The Crab-synth is an Combine heavy combat vehicle designed to suppress well-armed enemy groups and breach fortified positions. Despite its relatively squat build, the synthet possesses considerable mass and is capable of quickly moving across rough terrain using several powerful limbs.
+
+The main feature of this entity is its retractable minigun, concealed within its hull. Upon detecting a target, the weapon mechanism deploys, unleashing a dense stream of fire on the enemy. The minigun is particularly effective against groups of infantry, light vehicles, and targets that remain in one place for too long.
+
+In close combat, the Crab Synthent can use its own limbs as weapons. However, its primary role is not to hunt alone, but to support other synths. It typically operates in conjunction with Combine Assault Synths, which provide flanking cover and prevent the enemy from focusing all of its fire on the heavy vehicle.
+
+This tactic makes encounters with them particularly frustrating: while the Assault Synths keep the enemy on the move, the Crab Synthent methodically rakes the area with fire from its minigun. The Combine seems to really enjoy solving tactical problems by the "put another gun in."
+
+TIPS:
+
+- Don't try to out-shoot a minigun in the open.
+- Constantly change positions, especially if the synth has already deployed its weapon. - First, eliminate the accompanying Assault Synths if they interfere with your fire concentration.
+- Use cover and attack from the flanks or rear.
+- Powerful explosive weapons are especially effective against heavy hulls.
+- If a Crab Synth is left alone, don't relax: its firepower still allows it to quickly turn an open area into a very difficult place to walk.
---
+++ lua/netricsa/descriptions/fr/zb_crab_synth.lua
@@ -0,0 +1,30 @@
+COMBINEZ CRABE-SYNTHÉ
+
+
+CLASSE : Combiner Crabe-synthé
+MÉTABOLISME : Biomécanique Synthétique
+PERCEPTION : Unité d'assaut lourd
+TAILLE : 7,2 pieds
+ENDURANCE : Élevée
+HOSTILITÉ : Extrême
+ARMES : Minigun rétractable, griffes, bélier
+RÉCOMPENSE : 800 FC
+MENACE : Très élevée
+
+DESCRIPTIF :
+
+Le Crab-synth est un véhicule de combat lourd Combine conçu pour supprimer les groupes ennemis bien armés et percer les positions fortifiées. Malgré sa construction relativement trapue, le synthet possède une masse considérable et est capable de se déplacer rapidement sur un terrain accidenté à l'aide de plusieurs membres puissants.
+
+La principale caractéristique de cette entité est son minigun rétractable, dissimulé dans sa coque. Dès la détection d'une cible, le mécanisme de l'arme se déploie, déclenchant un flux de tirs dense sur l'ennemi. Le minigun est particulièrement efficace contre les groupes d'infanterie, les véhicules légers et les cibles qui restent trop longtemps au même endroit.
+
+En combat rapproché, le Crab Synthent peut utiliser ses propres membres comme armes. Cependant, son rôle premier n’est pas de chasser seul, mais de soutenir d’autres synthés. Il fonctionne généralement en conjonction avec des Combine Assault Synths, qui fournissent une couverture de flanc et empêchent l'ennemi de concentrer tous ses tirs sur le véhicule lourd.
+
+Cette tactique rend les rencontres avec eux particulièrement frustrantes : pendant que les Assault Synths maintiennent l'ennemi en mouvement, le Crab Synthent ratisse méthodiquement la zone avec le feu de son minigun. Le Combine semble vraiment aimer résoudre les problèmes tactiques en « mettant une autre arme ».
+
+CONSEILS :
+
+- N'essayez pas de tirer plus vite qu'un minigun à l'air libre.
+- Changez constamment de position, surtout si le synthé a déjà déployé son arme. - Tout d'abord, éliminez les synthés d'assaut qui les accompagnent s'ils interfèrent avec votre concentration de tir.
+- Utilisez une couverture et attaquez depuis les flancs ou l'arrière.
+- Les armes explosives puissantes sont particulièrement efficaces contre les coques lourdes.
+- Si un Crab Synth est laissé seul, ne vous relâchez pas : sa puissance de feu lui permet toujours de transformer rapidement une zone dégagée en un endroit très difficile à parcourir.
