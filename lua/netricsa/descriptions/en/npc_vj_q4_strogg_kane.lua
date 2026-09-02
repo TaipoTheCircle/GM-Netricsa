6d1fbdd395a977db257f2d2e212592f6546067f1
TaipoTheCircle <tails321supsen@gmail.com>
2026-09-02 15:40:31 +0300

    kane strogg

---
+++ lua/netricsa/descriptions/en/npc_vj_q4_strogg_kane.lua
@@ -0,0 +1,36 @@
+STROGG MATTHEW KANE FROM THE STROGGOS
+
+
+CLASS:      Strogg Matthew Kane
+METABOLISM: Cybernetic Human-Hybrid
+PERCEPTION: Strogg Tactical Infantry
+SIZE:       5,9 ft
+ENDURANCE:  High (Armor + Cybernetic Enhancements)
+HOSTILITY:  Extreme
+WEAPONS:    Shotgun, Machine Gun, Hyper Blaster, Railgun
+REWARD:     5200 FC
+THREAT:     High
+
+DESCRIPTION:
+
+This...Specimen... is quite unusual. A unique case among the Strogg. He was once a human and a corporal in the Space Marine Corps, serving in the elite unit "Rhino Squad." After being captured by the Strogg, Kane underwent a stroggification procedure, which resulted in a significant portion of his body being replaced with cybernetic components.
+
+However, Kane did not become a normal Strogg. His transformation was interrupted before the most crucial stage of the procedure was completed, preventing the neurocyte from fully taking control of his consciousness. As a result, Kane retained his will and continued to fight against those who had transformed him into a cyborg.
+
+After partial stroggification, his body became significantly stronger than a human's. Cybernetic legs increased his speed and physical endurance, and integrated systems allow him to interface with Strogg technology, including their terminals, teleporters, and medical stations. Furthermore, Kane is able to safely pass through some Strogg defensive laser systems that would be deadly to a normal human.
+
+In terms of combat capabilities, Kane is most similar to Tactical Strogg. This is especially noteworthy, as these units were created from the most highly trained individuals and designed to conduct complex combat operations. They use cover, flanking maneuvers, and cooperate with each other, rather than simply running toward the enemy.
+
+Kane, however, retains the skills of a seasoned soldier. He is capable of handling virtually all weapons available to him, quickly switching weapons depending on the situation, and using the environment against the enemy.
+
+But Kane's main distinction isn't his weaponry; it's his understanding of how humans think. And at the same time, he knows how the Strogg operate.
+
+Simply put, to the Strogg, he remains a malfunctioning element that somehow retains the ability to make decisions independently.
+
+TIPS:
+
+- His Strogg designation is 8462-9B.
+- Kane is an extremely valuable fighter for the human unit.
+- Don't mistake him for a regular Strogg. Despite his appearance, Kane fights on the side of the humans.
+- If, for any reason, Kane finds himself an enemy, the situation becomes significantly more difficult than when facing a regular Strogg.
+- Kane combines human tactics and Strogg capabilities.
---
+++ lua/netricsa/descriptions/fr/npc_vj_q4_strogg_kane.lua
@@ -0,0 +1,36 @@
+STROGG MATTHEW KANE DES STROGGOS
+
+
+CLASSE : Strogg Matthew Kane
+MÉTABOLISME : Humain-Hybride Cybernétique
+PERCEPTION : infanterie tactique Strogg
+TAILLE : 5,9 pieds
+ENDURANCE : Élevée (armure + améliorations cybernétiques)
+HOSTILITÉ : Extrême
+ARMES : Fusil de chasse, Mitrailleuse, Hyper Blaster, Railgun
+RÉCOMPENSE : 5200 FC
+MENACE : Élevée
+
+DESCRIPTIF :
+
+Ce... spécimen... est assez inhabituel. Un cas unique chez les Strogg. Il était autrefois humain et caporal dans le Space Marine Corps, servant dans l'unité d'élite « Rhino Squad ». Après avoir été capturé par les Strogg, Kane a subi une procédure de stroggification, qui a abouti au remplacement d'une partie importante de son corps par des composants cybernétiques.
+
+Cependant, Kane n’est pas devenu un Strogg normal. Sa transformation a été interrompue avant que l’étape la plus cruciale de la procédure ne soit terminée, empêchant le neurocyte de prendre pleinement le contrôle de sa conscience. En conséquence, Kane a conservé sa volonté et a continué à lutter contre ceux qui l'avaient transformé en cyborg.
+
+Après une stroggification partielle, son corps est devenu nettement plus fort que celui d'un humain. Les jambes cybernétiques ont augmenté sa vitesse et son endurance physique, et des systèmes intégrés lui permettent d'interagir avec la technologie Strogg, y compris leurs terminaux, téléporteurs et postes médicaux. De plus, Kane est capable de traverser en toute sécurité certains systèmes laser défensifs Strogg qui seraient mortels pour un humain normal.
+
+En termes de capacités de combat, Kane ressemble le plus à Tactical Strogg. Ceci est particulièrement remarquable, car ces unités ont été créées à partir des individus les plus hautement qualifiés et conçues pour mener des opérations de combat complexes. Ils utilisent la couverture, les manœuvres de flanc et coopèrent les uns avec les autres, plutôt que de simplement courir vers l'ennemi.
+
+Kane conserve cependant les compétences d’un soldat chevronné. Il est capable de manipuler pratiquement toutes les armes à sa disposition, de changer rapidement d'arme en fonction de la situation et d'utiliser l'environnement contre l'ennemi.
+
+Mais la principale distinction de Kane ne réside pas dans son armement ; c'est sa compréhension de la façon dont les humains pensent. Et en même temps, il sait comment fonctionnent les Strogg.
+
+En termes simples, pour le Strogg, il reste un élément défectueux qui conserve d’une manière ou d’une autre la capacité de prendre des décisions de manière indépendante.
+
+CONSEILS:
+
+- Sa désignation Strogg est 8462-9B.
+- Kane est un combattant extrêmement précieux pour l'unité humaine.
+- Ne le confondez pas avec un Strogg ordinaire. Malgré son apparence, Kane se bat aux côtés des humains.
+- Si, pour une raison quelconque, Kane se retrouve comme un ennemi, la situation devient nettement plus difficile que face à un Strogg ordinaire.
+- Kane combine tactiques humaines et capacités Strogg.
