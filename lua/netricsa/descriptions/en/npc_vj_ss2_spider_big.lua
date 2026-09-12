3c1d05713154efe0647d49a756f8943ebb450f38
TaipoTheCircle <tails321supsen@gmail.com>
2026-09-13 00:54:48 +0300

    spider big ss2

---
+++ lua/netricsa/descriptions/en/npc_vj_ss2_spider_big.lua
@@ -0,0 +1,24 @@
+BIOMECHANOID SPIDER, THE MOMMY FROM THE MAGNOR
+
+
+CLASS:       Biomechanoid Spider, The Mommy
+METABOLISM:  Endothermic Cyborg
+PERCEPTION:  Genetically Programmed
+SIZE:        51 ft
+ENDURANCE:   High
+HOSTILITY:   High
+WEAPONS:     Bite, Acid Spit
+REWARD:      750 FC
+THREAT:      High
+
+DESCRIPTION:
+
+Mental makes extensive use of spiders in his armies, particularly the Antares variety. Seeking to take his collection of giant spiders to the next level, he began creating a new breed-half-biological, half-cybernetic. Thus, after countless mutations, genetic engineering, scientist casualties, and shed tears in the lab, the Biomechanoid Spider was born.
+
+The The Mommies are significantly larger, tougher, and more dangerous than the Hatchlings. They lumber toward you while spitting acid projectiles. Try not to get hit by them.
+
+TIPS:
+
+- A few cannonballs are your quickest way to destroy them-unless you have a Serious Bomb.
+- Your next best option is a barrage of explosives.
+- Don't get close. Just don't. This is one spider you can't squash with your boot.
