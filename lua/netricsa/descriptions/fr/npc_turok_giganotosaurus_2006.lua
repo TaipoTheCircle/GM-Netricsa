49e30f6c3d46ba04e3105e159bdd9e3318bcdc19
TaipoTheCircle <tails321supsen@gmail.com>
2026-09-14 19:21:35 +0300

    gigantonosaurus 2006 and orcs

---
+++ lua/netricsa/descriptions/fr/npc_turok_giganotosaurus_2006.lua
@@ -0,0 +1,24 @@
+GIGANOTOSAURUS (2006) FROM THE TERRAFORMED PLANET
+
+
+CLASS:       Giganotosaurus (2006)
+METABOLISM:  Carnivore, Theropod
+BEHAVIOR:    Free-roaming Apex Predator
+SIZE:        23 ft
+STAMINA:     Extreme
+HOSTILITY:   Absolute
+WEAPONRY:    Jaws, Head-ramming, Vehicle Crushing
+REWARD:      9500 FC
+THREAT:      Absolute
+
+DESCRIPTIF :
+
+One of the most massive and heavy land predators on the planet; its power is enough to make even a Tyrannosaurus retreat. Unlike its more slender relatives, this subspecies possesses an incredibly wide, heavy skull and a bulky, dense build. Its unpatterned gray skin is covered in deep scars from past ferocious battles.
+
+It actively patrols the areas near Mendel-Gruman Corporation outposts. It exhibits unpredictable behavior: capable of chasing down heavy wheeled vehicles at high speed and easily destroying armored off-roaders.
+
+TIPS:
+
+- Conventional small arms have almost no effect on it.
+- Thanks to its predatory instincts, you can lure it into attacking Mendel-Gruman soldier patrols to clear a path for yourself.
+- If your jeep enters its field of vision, do not slow down: this monster can smash any vehicle-including the "Rhino 4x4"-to pieces in just a couple of hits.
---
+++ lua/netricsa/descriptions/fr/weapon_turok_submachinegun_dual.lua
@@ -0,0 +1,21 @@
+DUAL ORO FP9 LIGHT ASSAULT CARBINES
+
+
+ARME : Dual ORO FP9 Light Assault Carbines
+MUNITIONS : 9x19mm Parabellum
+ROF : 180 rounds / min
+
+DESCRIPTIF :
+
+This compact weapons was developed by ORO Defense as part of a program to standardize small arms for private military companies. The FP9 is based on legacy MP5 designs, but with an emphasis on reducing manufacturing costs: stamping instead of milling, and standardized components.
+
+It is manufactured using the same design as an automatic carbine chambered for pistol cartridges. The frame is one-piece stamped steel with a matte finish to reduce IR signature. The hammer-type trigger mechanism allows for single and continuous fire. It is fed from 30-round box magazines, located in front of the pistol grip.
+
+The open sights are non-adjustable and designed for a standard range of 100 meters. They are widely used by Whiskey Company units and Mendel-Gruman forces.
+
+The primary customer was Whiskey Company, which received the first batch for riot control on resource worlds. Later, extensive use by Mendel-Gruman forces on a terraformed planet was noted.
+
+REMARQUES :
+
+- A burst of 3-5 rounds is guaranteed to incapacitate a target.
+- The high rate of fire allows for suppression of a group of up to three people before they can take cover.
